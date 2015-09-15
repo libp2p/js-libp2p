470d8bfc6a2928fbb39e8ee36c3115411b3386c1
David Dias <daviddias.p@gmail.com>
2015-09-15 19:43:55 +0100

    after many solo-bikeshedding, figured out that I could make it way simpler

--- src/index.js
+++ src/index.js
@@ -1,46 +1,10 @@
 var tcp = require('net')
-var async = require('async')
 
 exports = module.exports
 
-exports.dial = function (peerInfo, callback) {
-  var socket
-  async.eachSeries(peerInfo.multiaddrs, function (multiaddr, next) {
-    if (!multiaddr.protoNames().indexOf('tcp')) {
-      return next()
-    }
-
-    if (socket) {
-      return next()
-    }
-
-    var tmp = tcp.connect(multiaddr.toOptions(), function connected () {
-      socket = tmp
-      next()
-    })
-
-    tmp.once('error', function (err) {
-      console.log(multiaddr.toString(), 'on',
-          peerInfo.id.toB58String(), 'not available', err)
-      next()
-    })
-  }, function done () {
-    if (!socket) {
-      return callback(new Error('Not able to open a scoket with peer - ',
-        peerInfo.id.toB58String()))
-    }
-    callback(null, socket)
-  })
+exports.dial = function (multiaddr, options) {
+  options.ready = options.ready || function noop () {}
+  return tcp.connect(multiaddr.toOptions(), options.ready)
 }
 
-exports.listen = function (options, callback, readyCallback) {
-  options.port = options.port || 4001
-
-  var listener = tcp.createServer(function (socket) {
-    callback(null, socket)
-  })
-
-  listener.listen(options.port, readyCallback)
-
-  return listener
-}
+exports.createListener = tcp.createServer
