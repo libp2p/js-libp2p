e5ad5418b25f10f69277862181a6c2e1def1da1d
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-03-22 11:05:57 -0700

    standard linting for examples

--- examples/listen.js
+++ examples/listen.js
@@ -1,13 +1,12 @@
 var multiplex = require('libp2p-multiplex')
 var net = require('net')
 
-var server = net.createServer(function (conn) {
+net.createServer(function (conn) {
   var multi = multiplex(conn, false)
 
   multi.newStream(echoService)
   multi.newStream(lengthService)
 }).listen(9000)
-
 
 function echoService (err, conn) {
   if (err) throw err
@@ -19,6 +18,6 @@ function echoService (err, conn) {
 function lengthService (err, conn) {
   if (err) throw err
   conn.on('data', function (data) {
-    conn.write(data.length+'\n')
+    conn.write(data.length + '\n')
   })
 }
