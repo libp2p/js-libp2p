57c85fe346ace7525584d4526293da335c5b287b
David Dias <daviddias.p@gmail.com>
2016-11-25 20:08:05 +0000

    chore: update oldie package.json

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,18 +2,11 @@
 'use strict'
 
 const expect = require('chai').expect
-const Swarm = require('libp2p-swarm')
-const Id = require('peer-id')
-const Peer = require('peer-info')
 
-const Node = require('../src')
+const libp2p = require('../src')
 
 describe('libp2p', () => {
-  it('can be instantiated', () => {
-    const peer = new Peer(Id.create(), [])
-    const swarm = new Swarm(peer)
-    const node = new Node(swarm)
-
-    expect(node.swarm).to.be.eql(swarm)
+  it('the skeleton is fine, now go build your own libp2p bundle', () => {
+    expect(libp2p).to.exist
   })
 })
