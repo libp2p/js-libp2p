1b8762078adecfe18219f19748f0e1b56b571fca
Nuno Nogueira <nunofmn@gmail.com>
2016-12-01 07:11:02 +0000

    Upgrade to use latest libp2p modules (#38)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ var Peer = require('peer-info')
 var multiaddr = require('multiaddr')
 var EventEmitter = require('events').EventEmitter
 var util = require('util')
+var debug = require('debug')
+
+var log = debug('libp2p:railing')
+log.error = debug('libp2p:railing:error')
 
 exports = module.exports = Bootstrap
 exports.default = require('./default.json')
@@ -23,18 +27,24 @@ function Bootstrap (peerList, options, swarm) {
       var mh = multiaddr(peerCandidate.split('/').splice(0, 5).join('/'))
       var peerId = Id.createFromB58String(peerCandidate.split('/').splice(6)[0])
 
-      var p = new Peer(peerId, [mh])
+      Peer.create(peerId, function (err, peer) {
+        if (err) {
+          return log.error('Error creating PeerInfo from bootstrap peer', err)
+        }
 
-      if (options && options.verify) {
-        swarm.dial(p, {}, function (err) {
-          if (err) {
-            return
-          }
-          self.emit('peer', p)
-        })
-      } else {
-        self.emit('peer', p)
-      }
+        peer.multiaddr.add(mh)
+
+        if (options && options.verify) {
+          swarm.dial(peer, function (err) {
+            if (err) {
+              return
+            }
+            self.emit('peer', peer)
+          })
+        } else {
+          self.emit('peer', peer)
+        }
+      })
     })
   })
 }
