9ee10ad72cd7c1e6e250d3cc4ebcf754fb034082
David Dias <daviddias.p@gmail.com>
2016-12-01 08:46:26 +0000

    fix: stop using lack of default memset as random number generator

---
+++ src/util.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const randomBytes = require('random-bytes')
+const config = require('./config')
+
+exports = module.exports
+exports.genPingValue = (length) => {
+  if (!length) {
+    length = config.PING_LENGTH
+  }
+  return randomBytes.sync(length)
+}
