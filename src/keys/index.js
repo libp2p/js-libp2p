c73adb00cc49b7c862634210570d5a799b98ecb1
David Dias <daviddias.p@gmail.com>
2017-02-10 18:53:36 -0800

    fix: add libp2p-crypto-secp256k1

--- src/keys/index.js
+++ src/keys/index.js
@@ -2,10 +2,6 @@
 
 module.exports = {
   rsa: require('./rsa'),
-  ed25519: require('./ed25519')
-}
-
-try {
-  module.exports.secp256k1 = require('libp2p-crypto-secp256k1')
-} catch (err) {
+  ed25519: require('./ed25519'),
+  secp256k1: require('libp2p-crypto-secp256k1')
 }
