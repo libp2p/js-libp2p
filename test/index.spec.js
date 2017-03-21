baee2b7945e9c48c8a00c6ef71769cc201179529
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-03-21 17:04:56 +0100

    chore(deps): update

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,14 @@
 /* eslint-env mocha */
 'use strict'
 
-const expect = require('chai').expect
+const chai = require('chai')
+chai.use(require('dirty-chai'))
+const expect = chai.expect
 
 const libp2p = require('../src')
 
 describe('libp2p', () => {
   it('the skeleton is fine, now go build your own libp2p bundle', () => {
-    expect(libp2p).to.exist
+    expect(libp2p).to.exist()
   })
 })
