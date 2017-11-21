585f13001a23143998a827e79cdd98aa5b0d404e
Pedro Teixeira <i@pgte.me>
2017-11-21 08:44:39 +0000

    multiaddr trim test

---
+++ test/multiaddr-trim.js
@@ -0,0 +1,32 @@
+/* eslint-env mocha */
+'use strict'
+
+const chai = require('chai')
+chai.use(require('dirty-chai'))
+const expect = chai.expect
+const createNode = require('./utils/node').createNode
+
+describe('multiaddr trim', () => {
+  let node
+  it('can create a test node with an irrelevant multiaddr', (done) => {
+    createNode(
+      [
+        '/ip4/0.0.0.0/tcp/0/p2p-webrtc-direct'
+      ],
+      (err, _node) => {
+        expect(err).to.not.exist()
+        node = _node
+        expect(node.peerInfo.multiaddrs.toArray()).to.have.length(1)
+        done()
+      })
+  })
+
+  it('starts node', (done) => {
+    node.start(done)
+  })
+
+  it('irrelevant multiaddr got trimmed', (done) => {
+    expect(node.peerInfo.multiaddrs.toArray()).to.have.length(0)
+    done()
+  })
+})
