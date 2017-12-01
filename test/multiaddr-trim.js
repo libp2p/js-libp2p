42940bcc98263fa61343c33f0444da6833fe4db2
Pedro Teixeira <i@pgte.me>
2017-12-01 10:20:48 +0000

    tests: testing multiaddress trim leaves relevant addresses untouched

--- test/multiaddr-trim.js
+++ test/multiaddr-trim.js
@@ -8,15 +8,25 @@ const createNode = require('./utils/node').createNode
 
 describe('multiaddr trim', () => {
   let node
+
+  after((done) => {
+    if (node) {
+      node.stop(done)
+    } else {
+      done()
+    }
+  })
+
   it('can create a test node with an irrelevant multiaddr', (done) => {
     createNode(
       [
-        '/ip4/0.0.0.0/tcp/0/p2p-webrtc-direct'
+        '/ip4/0.0.0.0/tcp/0/wss/p2p-webrtc-direct',
+        '/ip4/0.0.0.0/tcp/0'
       ],
       (err, _node) => {
         expect(err).to.not.exist()
         node = _node
-        expect(node.peerInfo.multiaddrs.toArray()).to.have.length(1)
+        expect(node.peerInfo.multiaddrs.toArray()).to.have.length(2)
         done()
       })
   })
@@ -26,7 +36,8 @@ describe('multiaddr trim', () => {
   })
 
   it('irrelevant multiaddr got trimmed', (done) => {
-    expect(node.peerInfo.multiaddrs.toArray()).to.have.length(0)
+    expect(node.peerInfo.multiaddrs.toArray()).to.have.length(1)
+    expect(node.peerInfo.multiaddrs.toArray()[0].toString()).to.match(/^\/ip4\/0.0.0.0\/tcp\/0\/ipfs\/\w+/)
     done()
   })
 })
