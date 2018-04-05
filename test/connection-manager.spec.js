5104f21b4d89730a04085fef930e69008fecc4b3
Pedro Teixeira <i@pgte.me>
2018-04-05 11:10:24 +0100

    tests: generalized preparation

--- test/connection-manager.spec.js
+++ test/connection-manager.spec.js
@@ -4,48 +4,12 @@
 const chai = require('chai')
 chai.use(require('dirty-chai'))
 const expect = chai.expect
-const each = require('async/each')
-const series = require('async/series')
-
-const createThing = require('./utils/create-thing')
-const connectAll = require('./utils/connect-all')
+const Prepare = require('./utils/prepare')
 
 describe('connection manager', function () {
-  let nodes
-
-  before((done) => {
-    series([
-      (cb) => createThing({}, cb),
-      (cb) => createThing({}, cb),
-      (cb) => createThing({}, cb)
-    ], (err, results) => {
-      expect(err).to.not.exist()
-      expect(results).to.have.length(3)
-      nodes = results
-      done()
-    })
-  })
-
-  before((done) => {
-    console.log('going to connect all!')
-    connectAll(nodes, (err) => {
-      if (err) {
-        return done(err)
-      }
-      console.log('CONNECTED ALL!!')
-      done()
-    })
-  })
-
-  after((done) => {
-    each(nodes, (node, cb) => {
-      node.connManager.stop()
-      series([
-        (cb) => node.libp2pNode.stop(cb),
-        (cb) => node.repo.teardown(cb)
-      ], cb)
-    }, done)
-  })
+  const prepare = Prepare(3)
+  before(prepare.before)
+  after(prepare.after)
 
   it('works', (done) => {
     setTimeout(done, 1900)
---
+++ test/utils/prepare.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const chai = require('chai')
+chai.use(require('dirty-chai'))
+const expect = chai.expect
+
+const waterfall = require('async/waterfall')
+const series = require('async/series')
+const each = require('async/each')
+
+const createThing = require('./create-thing')
+const connectAll = require('./connect-all')
+
+module.exports = (count) => {
+
+  let nodes
+
+  const before = (done) => {
+    waterfall([
+      (cb) => {
+        const tasks = []
+        for (let i = 0; i < count; i++) {
+          tasks.push((cb) => createThing({}, cb))
+        }
+
+        series(tasks, cb)
+      },
+      (things, cb) => {
+        expect(things.length).to.equal(count)
+        nodes = things
+        connectAll(things, (err) => cb(err, things))
+      }
+    ], done)
+  }
+
+  const after = (done) => {
+    each(nodes, (node, cb) => {
+      node.connManager.stop()
+      series([
+        (cb) => node.libp2pNode.stop(cb),
+        (cb) => node.repo.teardown(cb)
+      ], cb)
+    }, done)
+  }
+
+  return { before, after }
+}
