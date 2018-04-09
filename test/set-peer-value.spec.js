cdfec34fce470d00f9c6298ffde7a9d440b5891f
Pedro Teixeira <i@pgte.me>
2018-04-09 12:01:16 +0100

    tests: testing setPeerValue

---
+++ test/set-peer-value.spec.js
@@ -0,0 +1,46 @@
+/* eslint-env mocha */
+'use strict'
+
+const chai = require('chai')
+chai.use(require('dirty-chai'))
+const expect = chai.expect
+
+const Prepare = require('./utils/prepare')
+
+const PEER_COUNT = 3
+
+describe('setPeerValue', function () {
+  const prepare = Prepare(PEER_COUNT, [{
+    maxPeers: 1
+  }])
+  before(prepare.create)
+  after(prepare.after)
+
+  it('kicks out lower valued peer first', function (done) {
+    this.timeout(10000)
+
+    let disconnects = 0
+    let firstConnectedPeer
+    const manager = prepare.connManagers()[0]
+
+    manager.once('connected', (peerId) => {
+      if (!firstConnectedPeer) {
+        firstConnectedPeer = peerId
+        manager.setPeerValue(peerId, 1)
+      } else {
+        manager.setPeerValue(peerId, 0)
+      }
+    })
+
+    manager.on('disconnected', (peerId) => {
+      disconnects++
+      expect(disconnects).to.be.most(PEER_COUNT - 2)
+      expect(peerId).to.not.be.equal(firstConnectedPeer)
+      done()
+    })
+
+    prepare.tryConnectAll((err) => {
+      expect(err).to.not.exist()
+    })
+  })
+})
