e7b17593c627bb1a4c8ae5c623bb863426a0f0e7
Alan Shaw <alan@tableflip.io>
2018-06-28 15:01:50 +0100

    test: add tests for peer discovery module registration
    License: MIT
    Signed-off-by: Alan Shaw <alan@tableflip.io>

--- test/peer-discovery.node.js
+++ test/peer-discovery.node.js
@@ -4,6 +4,7 @@
 const chai = require('chai')
 chai.use(require('dirty-chai'))
 const expect = chai.expect
+const sinon = require('sinon')
 const signalling = require('libp2p-webrtc-star/src/sig-server')
 const parallel = require('async/parallel')
 const crypto = require('crypto')
@@ -57,6 +58,163 @@ describe('peer discovery', () => {
       ], done)
     })
   }
+
+  describe('module registration', () => {
+    it('should enable module by default', (done) => {
+      const mockDiscovery = {
+        on: sinon.stub(),
+        start: sinon.stub().callsArg(0),
+        stop: sinon.stub().callsArg(0)
+      }
+
+      const options = { modules: { peerDiscovery: [ mockDiscovery ] } }
+
+      createNode(['/ip4/0.0.0.0/tcp/0'], options, (err, node) => {
+        expect(err).to.not.exist()
+
+        node.start((err) => {
+          expect(err).to.not.exist()
+          expect(mockDiscovery.start.called).to.be.true()
+          node.stop(done)
+        })
+      })
+    })
+
+    it('should enable module by configutation', (done) => {
+      const mockDiscovery = {
+        on: sinon.stub(),
+        start: sinon.stub().callsArg(0),
+        stop: sinon.stub().callsArg(0),
+        tag: 'mockDiscovery'
+      }
+
+      const enabled = sinon.stub().returns(true)
+
+      const options = {
+        modules: { peerDiscovery: [ mockDiscovery ] },
+        config: {
+          peerDiscovery: {
+            mockDiscovery: {
+              get enabled () {
+                return enabled()
+              }
+            }
+          }
+        }
+      }
+
+      createNode(['/ip4/0.0.0.0/tcp/0'], options, (err, node) => {
+        expect(err).to.not.exist()
+
+        node.start((err) => {
+          expect(err).to.not.exist()
+          expect(mockDiscovery.start.called).to.be.true()
+          expect(enabled.called).to.be.true()
+          node.stop(done)
+        })
+      })
+    })
+
+    it('should disable module by configutation', (done) => {
+      const mockDiscovery = {
+        on: sinon.stub(),
+        start: sinon.stub().callsArg(0),
+        stop: sinon.stub().callsArg(0),
+        tag: 'mockDiscovery'
+      }
+
+      const disabled = sinon.stub().returns(false)
+
+      const options = {
+        modules: { peerDiscovery: [ mockDiscovery ] },
+        config: {
+          peerDiscovery: {
+            mockDiscovery: {
+              get enabled () {
+                return disabled()
+              }
+            }
+          }
+        }
+      }
+
+      createNode(['/ip4/0.0.0.0/tcp/0'], options, (err, node) => {
+        expect(err).to.not.exist()
+
+        node.start((err) => {
+          expect(err).to.not.exist()
+          expect(mockDiscovery.start.called).to.be.false()
+          expect(disabled.called).to.be.true()
+          node.stop(done)
+        })
+      })
+    })
+
+    it('should register module passed as function', (done) => {
+      const mockDiscovery = {
+        on: sinon.stub(),
+        start: sinon.stub().callsArg(0),
+        stop: sinon.stub().callsArg(0)
+      }
+
+      const MockDiscovery = sinon.stub().returns(mockDiscovery)
+      MockDiscovery.tag = 'mockDiscovery'
+
+      const options = {
+        modules: { peerDiscovery: [ MockDiscovery ] },
+        config: {
+          peerDiscovery: {
+            mockDiscovery: {
+              enabled: true,
+              time: Date.now()
+            }
+          }
+        }
+      }
+
+      createNode(['/ip4/0.0.0.0/tcp/0'], options, (err, node) => {
+        expect(err).to.not.exist()
+
+        node.start((err) => {
+          expect(err).to.not.exist()
+          expect(mockDiscovery.start.called).to.be.true()
+          expect(MockDiscovery.called).to.be.true()
+          // Ensure configuration was passed
+          expect(MockDiscovery.firstCall.args[0])
+            .to.deep.include(options.config.peerDiscovery.mockDiscovery)
+          node.stop(done)
+        })
+      })
+    })
+
+    it('should register module passed as object', (done) => {
+      const mockDiscovery = {
+        on: sinon.stub(),
+        start: sinon.stub().callsArg(0),
+        stop: sinon.stub().callsArg(0),
+        tag: 'mockDiscovery'
+      }
+
+      const options = {
+        modules: { peerDiscovery: [ mockDiscovery ] },
+        config: {
+          peerDiscovery: {
+            mockDiscovery: { enabled: true }
+          }
+        }
+      }
+
+      createNode(['/ip4/0.0.0.0/tcp/0'], options, (err, node) => {
+        expect(err).to.not.exist()
+
+        node.start((err) => {
+          expect(err).to.not.exist()
+          expect(mockDiscovery.start.called).to.be.true()
+          node.stop(done)
+        })
+      })
+    })
+  })
 
   describe('MulticastDNS', () => {
     setup({
