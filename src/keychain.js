0065b0a49e1b5403cda2ef77d4d9a94f00c01c73
Masahiro Saito <camelmasa@gmail.com>
2018-06-30 07:38:19 +0900

    chore: fix out of date npms (#21)

--- src/keychain.js
+++ src/keychain.js
@@ -2,7 +2,7 @@
 'use strict'
 
 const sanitize = require('sanitize-filename')
-const deepmerge = require('deepmerge')
+const deepmerge = require('lodash.merge')
 const crypto = require('libp2p-crypto')
 const DS = require('interface-datastore')
 const pull = require('pull-stream')
@@ -104,7 +104,8 @@ class Keychain {
     }
     this.store = store
 
-    const opts = deepmerge(defaultOptions, options)
+    const opts = {}
+    deepmerge(opts, defaultOptions, options)
 
     // Enforce NIST SP 800-132
     if (!opts.passPhrase || opts.passPhrase.length < 20) {
