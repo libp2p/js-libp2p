50b30a0a142b7b4634d336766cff33f1ac780e0e
Jacob Heun <jacobheun@gmail.com>
2018-08-17 16:00:02 +0200

    test: add tests for delegate peer routing

--- test/peer-routing.node.js
+++ test/peer-routing.node.js
@@ -8,7 +8,11 @@ chai.use(require('dirty-chai'))
 const expect = chai.expect
 const parallel = require('async/parallel')
 const _times = require('lodash.times')
+const DelegatedPeerRouter = require('libp2p-delegated-peer-routing')
+const sinon = require('sinon')
+const nock = require('nock')
 
+const jsonPeerId = require('./fixtures/test-peer.json')
 const createNode = require('./utils/create-node')
 
 describe('.peerRouting', () => {
@@ -19,9 +23,7 @@ describe('.peerRouting', () => {
     let nodeD
     let nodeE
 
-    before('create the outer ring of connections', function (done) {
-      this.timeout(5 * 1000)
-
+    before('create the outer ring of connections', (done) => {
       const tasks = _times(5, () => (cb) => {
         createNode('/ip4/0.0.0.0/tcp/0', {
           config: {
@@ -67,6 +69,15 @@ describe('.peerRouting', () => {
       ], done)
     })
 
+    it('should use the nodes dht', (done) => {
+      const stub = sinon.stub(nodeA._dht, 'findPeer').callsFake(() => {
+        stub.restore()
+        done()
+      })
+
+      nodeA.peerRouting.findPeer()
+    })
+
     describe('connected in an el ring', () => {
       it('should be able to find a peer we are not directly connected to', (done) => {
         parallel([
@@ -82,6 +93,108 @@ describe('.peerRouting', () => {
             done()
           })
         })
+      })
+    })
+  })
+
+  describe('via a delegate', () => {
+    let nodeA
+    let delegate
+
+    before((done) => {
+      parallel([
+        // Create the node using the delegate
+        (cb) => {
+          delegate = new DelegatedPeerRouter({
+            host: 'ipfs.io',
+            protocol: 'https',
+            port: '443'
+          })
+          createNode('/ip4/0.0.0.0/tcp/0', {
+            modules: {
+              peerRouting: delegate
+            },
+            config: {
+              EXPERIMENTAL: {
+                dht: true
+              }
+            }
+          }, (err, node) => {
+            expect(err).to.not.exist()
+            nodeA = node
+            nodeA.start(cb)
+          })
+        }
+      ], done)
+    })
+
+    afterEach(nock.cleanAll)
+
+    it('should use the delegate router to find peers', (done) => {
+      const stub = sinon.stub(delegate, 'findPeer').callsFake(() => {
+        stub.restore()
+        done()
+      })
+      nodeA.peerRouting.findPeer()
+    })
+
+    it('should be able to find a peer', (done) => {
+      const peerKey = 'key of a peer on the network'
+      const mockApi = nock('https://ipfs.io')
+        .post('/api/v0/dht/findpeer')
+        .query({
+          'arg': peerKey,
+          'stream-channels': true
+        })
+        .reply(200, `{"Extra":"","ID":"some other id","Responses":null,"Type":0}\n{"Extra":"","ID":"","Responses":[{"Addrs":["/ip4/127.0.0.1/tcp/4001"],"ID":"${peerKey}"}],"Type":2}\n`, [
+          'Content-Type', 'application/json',
+          'X-Chunked-Output', '1'
+        ])
+
+      nodeA.peerRouting.findPeer(peerKey, (err, peerInfo) => {
+        expect(err).to.not.exist()
+        expect(peerInfo.id).to.equal(peerKey)
+        expect(mockApi.isDone()).to.equal(true)
+        done()
+      })
+    })
+
+    it('should error when a peer cannot be found', (done) => {
+      const peerKey = 'key of a peer not on the network'
+      const mockApi = nock('https://ipfs.io')
+        .post('/api/v0/dht/findpeer')
+        .query({
+          'arg': peerKey,
+          'stream-channels': true
+        })
+        .reply(200, `{"Extra":"","ID":"some other id","Responses":null,"Type":6}\n{"Extra":"","ID":"yet another id","Responses":null,"Type":0}\n{"Extra":"routing:not found","ID":"","Responses":null,"Type":3}\n`, [
+          'Content-Type', 'application/json',
+          'X-Chunked-Output', '1'
+        ])
+
+      nodeA.peerRouting.findPeer(peerKey, (err, peerInfo) => {
+        expect(err).to.exist()
+        expect(peerInfo).to.not.exist()
+        expect(mockApi.isDone()).to.equal(true)
+        done()
+      })
+    })
+
+    it('should handle errors from the api', (done) => {
+      const peerKey = 'key of a peer not on the network'
+      const mockApi = nock('https://ipfs.io')
+        .post('/api/v0/dht/findpeer')
+        .query({
+          'arg': peerKey,
+          'stream-channels': true
+        })
+        .reply(502)
+
+      nodeA.peerRouting.findPeer(peerKey, (err, peerInfo) => {
+        expect(err).to.exist()
+        expect(peerInfo).to.not.exist()
+        expect(mockApi.isDone()).to.equal(true)
+        done()
       })
     })
   })
