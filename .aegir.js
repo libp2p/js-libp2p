0838dd7e0cdb812d4edda8bb7a72e278a942fc47
Jacob Heun <jacobheun@gmail.com>
2019-08-08 15:35:29 +0200

    test: add aegir steps for switch browser

--- .aegir.js
+++ .aegir.js
@@ -3,6 +3,15 @@
 const pull = require('pull-stream')
 const WebSocketStarRendezvous = require('libp2p-websocket-star-rendezvous')
 const sigServer = require('libp2p-webrtc-star/src/sig-server')
+const promisify = require('promisify-es6')
+const mplex = require('pull-mplex')
+const spdy = require('libp2p-spdy')
+const PeerBook = require('peer-book')
+const PeerId = require('peer-id')
+const PeerInfo = require('peer-info')
+const path = require('path')
+const Switch = require('libp2p-switch')
+const WebSockets = require('libp2p-websockets')
 
 const Node = require('./test/utils/bundle-nodejs.js')
 const {
@@ -15,9 +24,67 @@ let wrtcRendezvous
 let wsRendezvous
 let node
 let peerInfo
+let switchA
+let switchB
+
+function echo (protocol, conn) { pull(conn, conn) }
+function idJSON (id) {
+  const p = path.join(__dirname, `./test/switch/test-data/id-${id}.json`)
+  return require(p)
+}
+
+function createSwitchA () {
+  return new Promise((resolve, reject) => {
+    PeerId.createFromJSON(idJSON(1), (err, id) => {
+      if (err) { return reject(err) }
+
+      const peerA = new PeerInfo(id)
+      const maA = '/ip4/127.0.0.1/tcp/15337/ws'
+
+      peerA.multiaddrs.add(maA)
+      const sw = new Switch(peerA, new PeerBook())
+
+      sw.transport.add('ws', new WebSockets())
+      sw.start((err) => {
+        if (err) { return reject(err) }
+        resolve(sw)
+      })
+    })
+  })
+}
+
+function createSwitchB () {
+  return new Promise((resolve, reject) => {
+    PeerId.createFromJSON(idJSON(2), (err, id) => {
+      if (err) { return reject(err) }
+
+      const peerB = new PeerInfo(id)
+      const maB = '/ip4/127.0.0.1/tcp/15347/ws'
+
+      peerB.multiaddrs.add(maB)
+      const sw = new Switch(peerB, new PeerBook())
+
+      sw.transport.add('ws', new WebSockets())
+      sw.connection.addStreamMuxer(mplex)
+      sw.connection.addStreamMuxer(spdy)
+      sw.connection.reuse()
+      sw.handle('/echo/1.0.0', echo)
+      sw.start((err) => {
+        if (err) { return reject(err) }
+        resolve(sw)
+      })
+    })
+  })
+}
 
 const before = async () => {
-  [wrtcRendezvous, wsRendezvous, peerInfo] = await Promise.all([
+  [
+    wrtcRendezvous,
+    wsRendezvous,
+    peerInfo,
+    switchA,
+    switchB
+  ] = await Promise.all([
     sigServer.start({
       port: WRTC_RENDEZVOUS_MULTIADDR.nodeAddress().port
       // cryptoChallenge: true TODO: needs https://github.com/libp2p/js-libp2p-webrtc-star/issues/128
@@ -28,7 +95,9 @@ const before = async () => {
       strictMultiaddr: false,
       cryptoChallenge: true
     }),
-    getPeerRelay()
+    getPeerRelay(),
+    createSwitchA(),
+    createSwitchB()
   ])
 
   node = new Node({
@@ -52,7 +121,9 @@ const after = () => {
   return Promise.all([
     wrtcRendezvous.stop(),
     wsRendezvous.stop(),
-    node.stop()
+    node.stop(),
+    promisify(switchA.stop, { context: switchA })(),
+    promisify(switchB.stop, { context: switchB })()
   ])
 }
 
