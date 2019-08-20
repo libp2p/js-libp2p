6c4015a005fcb0d5e0475512bf8dfcaa039f2dc5
Jacob Heun <jacobheun@gmail.com>
2019-08-20 17:05:15 +0200

    chore: remove webrtcsupport

--- test/switch/browser.js
+++ test/switch/browser.js
@@ -1,12 +1,12 @@
 /* eslint-env mocha */
 'use strict'
 
-const w = require('webrtcsupport')
+const wrtcSupport = self.RTCPeerConnection && ('createDataChannel' in self.RTCPeerConnection.prototype)
 
 require('./transports.browser.js')
 require('./swarm-muxing+websockets.browser')
 
-if (w.support) {
+if (wrtcSupport) {
   require('./t-webrtc-star.browser')
   require('./swarm-muxing+webrtc-star.browser')
 }
