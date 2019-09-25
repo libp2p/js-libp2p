3b3fcead8fa3cf1eaec8a32b9c6105e0bd91e91a
Topper Bowers <topper@toppingdesign.com>
2019-09-25 19:10:56 +0200

    feat: allow inline public keys in messages

--- test/sign.spec.js
+++ test/sign.spec.js
@@ -56,6 +56,42 @@ describe('message signing', () => {
     })
   })
 
+  it('should be able to extract the public key from an inlined key', (done) => {
+    const testSecp256k1 = (peerId) => {
+      const message = {
+        from: peerId.id,
+        data: 'hello',
+        seqno: randomSeqno(),
+        topicIDs: ['test-topic']
+      }
+
+      const bytesToSign = Buffer.concat([SignPrefix, Message.encode(message)])
+      peerId.privKey.sign(bytesToSign, (err, expectedSignature) => {
+        if (err) return done(err)
+
+        signMessage(peerId, message, (err, signedMessage) => {
+          if (err) return done(err)
+
+          // Check the signature and public key
+          expect(signedMessage.signature).to.eql(expectedSignature)
+          signedMessage.key = undefined
+
+          // Verify the signature
+          verifySignature(signedMessage, (err, verified) => {
+            expect(err).to.not.exist()
+            expect(verified).to.eql(true)
+            done(err)
+          })
+        })
+      })
+    }
+
+    PeerId.create({ keyType: 'secp256k1', bits: 256 }, (err, peerId) => {
+      expect(err).to.not.exist()
+      testSecp256k1(peerId)
+    })
+  })
+
   it('should be able to extract the public key from the message', (done) => {
     const message = {
       from: peerId.id,
