b488b85a6fc3acb2c67857fea9fe3843d18c0bc6
Dirk McCormick <dirkmdev@gmail.com>
2019-11-25 17:53:43 -0500

    fix: async await examples/echo

--- examples/echo/src/dialer.js
+++ examples/echo/src/dialer.js
@@ -8,51 +8,54 @@
 const PeerId = require('peer-id')
 const PeerInfo = require('peer-info')
 const Node = require('./libp2p-bundle')
-const pull = require('pull-stream')
-const async = require('async')
+const pipe = require('it-pipe')
 
-async.parallel([
-  (cb) => PeerId.createFromJSON(require('./id-d'), cb),
-  (cb) => PeerId.createFromJSON(require('./id-l'), cb)
-], (err, ids) => {
-  if (err) { throw err }
+async function run() {
+  const [dialerId, listenerId] = await Promise.all([
+    PeerId.createFromJSON(require('./id-d')),
+    PeerId.createFromJSON(require('./id-l'))
+  ])
 
   // Dialer
-  const dialerId = ids[0]
   const dialerPeerInfo = new PeerInfo(dialerId)
   dialerPeerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
   const dialerNode = new Node({
     peerInfo: dialerPeerInfo
   })
 
-  // Peer to Dial
-  const listenerPeerInfo = new PeerInfo(ids[1])
-  const listenerId = ids[1]
+  // Peer to Dial (the listener)
+  const listenerPeerInfo = new PeerInfo(listenerId)
   const listenerMultiaddr = '/ip4/127.0.0.1/tcp/10333/p2p/' +
       listenerId.toB58String()
   listenerPeerInfo.multiaddrs.add(listenerMultiaddr)
 
-  dialerNode.start((err) => {
-    if (err) { throw err }
+  // Start the dialer libp2p node
+  await dialerNode.start()
 
-    console.log('Dialer ready, listening on:')
-    dialerPeerInfo.multiaddrs.forEach((ma) => console.log(ma.toString() +
-          '/p2p/' + dialerId.toB58String()))
+  console.log('Dialer ready, listening on:')
+  dialerPeerInfo.multiaddrs.forEach((ma) => console.log(ma.toString() +
+        '/p2p/' + dialerId.toB58String()))
 
-    console.log('Dialing to peer:', listenerMultiaddr.toString())
-    dialerNode.dialProtocol(listenerPeerInfo, '/echo/1.0.0', (err, conn) => {
-      if (err) { throw err }
+  // Dial the listener node
+  console.log('Dialing to peer:', listenerMultiaddr.toString())
+  const { stream } = await dialerNode.dialProtocol(listenerPeerInfo, '/echo/1.0.0')
 
-      console.log('nodeA dialed to nodeB on protocol: /echo/1.0.0')
+  console.log('nodeA dialed to nodeB on protocol: /echo/1.0.0')
 
-      pull(
-        pull.values(['hey']),
-        conn,
-        pull.collect((err, data) => {
-          if (err) { throw err }
-          console.log('received echo:', data.toString())
-        })
-      )
-    })
-  })
-})
+  pipe(
+    // Source data
+    ['hey'],
+    // Write to the stream, and pass its output to the next function
+    stream,
+    // Sink function
+    async function (source) {
+      // For each chunk of data
+      for await (const data of source) {
+        // Output the data
+        console.log('received echo:', data.toString())
+      }
+    }
+  )
+}
+
+run()
