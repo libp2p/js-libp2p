4ca569ca9692d82f02d60abed5b028419b783f25
Vasco Santos <vasco.santos@moxy.studio>
2019-11-28 22:50:14 +0100

    feat: discovery modules (#486)
    * feat: discovery modules
    
    * chore: address review

--- test/peer-discovery/index.spec.js
+++ test/peer-discovery/index.spec.js
@@ -5,7 +5,11 @@ const chai = require('chai')
 chai.use(require('dirty-chai'))
 const { expect } = chai
 const sinon = require('sinon')
+
 const defer = require('p-defer')
+const mergeOptions = require('merge-options')
+
+const MulticastDNS = require('libp2p-mdns')
 
 const Libp2p = require('../../src')
 const baseOptions = require('../utils/base-options.browser')
@@ -22,6 +26,7 @@ describe('peer discovery', () => {
 
   afterEach(async () => {
     libp2p && await libp2p.stop()
+    sinon.reset()
   })
 
   it('should dial know peers on startup', async () => {
@@ -41,5 +46,21 @@ describe('peer discovery', () => {
     libp2p.start()
     await deferred.promise
     expect(spy.getCall(0).args).to.eql([remotePeerInfo])
+  })
+
+  it('should ignore self on discovery', async () => {
+    libp2p = new Libp2p(mergeOptions(baseOptions, {
+      peerInfo,
+      modules: {
+        peerDiscovery: [MulticastDNS]
+      }
+    }))
+
+    await libp2p.start()
+    const discoverySpy = sinon.spy()
+    libp2p.on('peer:discovery', discoverySpy)
+    libp2p._discovery[0].emit('peer', libp2p.peerInfo)
+
+    expect(discoverySpy.called).to.eql(false)
   })
 })
