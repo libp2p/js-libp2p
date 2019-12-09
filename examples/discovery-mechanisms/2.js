980ae89336d135787cf18462500aec7755f580c9
Vasco Santos <vasco.santos@moxy.studio>
2019-12-09 10:21:27 +0100

    refactor: examples-discovery-mechanisms

--- examples/discovery-mechanisms/2.js
+++ examples/discovery-mechanisms/2.js
@@ -1,63 +1,49 @@
 /* eslint-disable no-console */
 'use strict'
 
-const libp2p = require('../../')
+const Libp2p = require('../../')
 const TCP = require('libp2p-tcp')
 const Mplex = require('libp2p-mplex')
 const SECIO = require('libp2p-secio')
 const PeerInfo = require('peer-info')
 const MulticastDNS = require('libp2p-mdns')
-const waterfall = require('async/waterfall')
-const parallel = require('async/parallel')
-const defaultsDeep = require('@nodeutils/defaults-deep')
 
-class MyBundle extends libp2p {
-  constructor (_options) {
-    const defaults = {
-      modules: {
-        transport: [ TCP ],
-        streamMuxer: [ Mplex ],
-        connEncryption: [ SECIO ],
-        peerDiscovery: [ MulticastDNS ]
-      },
-      config: {
-        peerDiscovery: {
-          mdns: {
-            interval: 20e3,
-            enabled: true
-          }
+const createNode = async () => {
+  const peerInfo = await PeerInfo.create()
+  peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
+
+  const node = Libp2p.create({
+    peerInfo,
+    modules: {
+      transport: [TCP],
+      streamMuxer: [Mplex],
+      connEncryption: [SECIO],
+      peerDiscovery: [MulticastDNS]
+    },
+    config: {
+      peerDiscovery: {
+        mdns: {
+          interval: 20e3,
+          enabled: true
         }
       }
     }
+  })
 
-    super(defaultsDeep(_options, defaults))
-  }
+  return node
 }
 
-function createNode (callback) {
-  let node
-
-  waterfall([
-    (cb) => PeerInfo.create(cb),
-    (peerInfo, cb) => {
-      peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
-      node = new MyBundle({
-        peerInfo
-      })
-      node.start(cb)
-    }
-  ], (err) => callback(err, node))
-}
-
-parallel([
-  (cb) => createNode(cb),
-  (cb) => createNode(cb)
-], (err, nodes) => {
-  if (err) { throw err }
-
-  const node1 = nodes[0]
-  const node2 = nodes[1]
+;(async () => {
+  const [node1, node2] = await Promise.all([
+    createNode(),
+    createNode()
+  ])
 
   node1.on('peer:discovery', (peer) => console.log('Discovered:', peer.id.toB58String()))
   node2.on('peer:discovery', (peer) => console.log('Discovered:', peer.id.toB58String()))
-})
+
+  await Promise.all([
+    node1.start(),
+    node2.start()
+  ])
+})();
