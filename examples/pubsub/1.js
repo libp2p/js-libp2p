9f0f08f58638a5e11318f3f1041ea5b5b0361e64
Vasco Santos <vasco.santos@moxy.studio>
2019-12-16 22:28:35 +0000

    refactor: examples-pubsub (#504)

--- examples/pubsub/1.js
+++ examples/pubsub/1.js
@@ -1,103 +1,51 @@
 /* eslint-disable no-console */
 'use strict'
 
-const libp2p = require('../../')
+const Libp2p = require('../../')
 const TCP = require('libp2p-tcp')
 const Mplex = require('libp2p-mplex')
 const SECIO = require('libp2p-secio')
 const PeerInfo = require('peer-info')
-const MulticastDNS = require('libp2p-mdns')
 const Gossipsub = require('libp2p-gossipsub')
-const defaultsDeep = require('@nodeutils/defaults-deep')
-const waterfall = require('async/waterfall')
-const parallel = require('async/parallel')
-const series = require('async/series')
 
-class MyBundle extends libp2p {
-  constructor (_options) {
-    const defaults = {
-      modules: {
-        transport: [ TCP ],
-        streamMuxer: [ Mplex ],
-        connEncryption: [ SECIO ],
-        peerDiscovery: [ MulticastDNS ],
-        pubsub: Gossipsub
-      },
-      config: {
-        peerDiscovery: {
-          mdns: {
-            interval: 2000,
-            enabled: true
-          }
-        },
-        pubsub: {
-          enabled: true,
-          emitSelf: true
-        }
-      }
+const createNode = async () => {
+  const peerInfo = await PeerInfo.create()
+  peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
+
+  const node = await Libp2p.create({
+    peerInfo,
+    modules: {
+      transport: [TCP],
+      streamMuxer: [Mplex],
+      connEncryption: [SECIO],
+      pubsub: Gossipsub
     }
-
-    super(defaultsDeep(_options, defaults))
-  }
-}
-
-function createNode (callback) {
-  let node
-
-  waterfall([
-    (cb) => PeerInfo.create(cb),
-    (peerInfo, cb) => {
-      peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
-      node = new MyBundle({
-        peerInfo
-      })
-      node.start(cb)
-    }
-  ], (err) => callback(err, node))
-}
-
-parallel([
-  (cb) => createNode(cb),
-  (cb) => createNode(cb)
-], (err, nodes) => {
-  if (err) { throw err }
-
-  const node1 = nodes[0]
-  const node2 = nodes[1]
-
-  node1.once('peer:connect', (peer) => {
-    console.log('connected to %s', peer.id.toB58String())
-
-    series([
-      // node1 subscribes to "news"
-      (cb) => node1.pubsub.subscribe(
-        'news',
-        (msg) => console.log(`node1 received: ${msg.data.toString()}`),
-        cb
-      ),
-      (cb) => setTimeout(cb, 500),
-      // node2 subscribes to "news"
-      (cb) => node2.pubsub.subscribe(
-        'news',
-        (msg) => console.log(`node2 received: ${msg.data.toString()}`),
-        cb
-      ),
-      (cb) => setTimeout(cb, 500),
-      // node2 publishes "news" every second
-      (cb) => {
-        setInterval(() => {
-          node2.pubsub.publish(
-            'news',
-            Buffer.from('Bird bird bird, bird is the word!'),
-            (err) => {
-              if (err) { throw err }
-            }
-          )
-        }, 1000)
-        cb()
-      },
-    ], (err) => {
-      if (err) { throw err }
-    })
   })
-})
+
+  await node.start()
+  return node
+}
+
+;(async () => {
+  const topic = 'news'
+
+  const [node1, node2] = await Promise.all([
+    createNode(),
+    createNode(),
+  ])
+
+  await node1.dial(node2.peerInfo)
+
+  await node1.pubsub.subscribe(topic, (msg) => {
+    console.log(`node1 received: ${msg.data.toString()}`)
+  })
+
+  await node2.pubsub.subscribe(topic, (msg) => {
+    console.log(`node2 received: ${msg.data.toString()}`)
+  })
+
+  // node2 publishes "news" every second
+  setInterval(() => {
+    node2.pubsub.publish(topic, Buffer.from('Bird bird bird, bird is the word!'))
+  }, 1000)
+})();
