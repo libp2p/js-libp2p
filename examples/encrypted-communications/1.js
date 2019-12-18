506af15b6b5c035efefae51bd2b0d4ace287da32
Vasco Santos <vasco.santos@moxy.studio>
2019-12-18 02:35:49 +0000

    refactor: examples/encrypted-communications (#499)
    * refactor: examples/encrypted-communications
    
    * chore: address review

--- examples/encrypted-communications/1.js
+++ examples/encrypted-communications/1.js
@@ -1,63 +1,52 @@
 'use strict'
 
-const libp2p = require('../../')
+const Libp2p = require('../../')
 const TCP = require('libp2p-tcp')
-const SPDY = require('libp2p-spdy')
+const Mplex = require('libp2p-mplex')
 const SECIO = require('libp2p-secio')
 const PeerInfo = require('peer-info')
-const waterfall = require('async/waterfall')
-const parallel = require('async/parallel')
-const pull = require('pull-stream')
-const defaultsDeep = require('@nodeutils/defaults-deep')
 
-class MyBundle extends libp2p {
-  constructor (_options) {
-    const defaults = {
-      modules: {
-        transport: [ TCP ],
-        streamMuxer: [ SPDY ],
-        connEncryption: [ SECIO ]
+const pipe = require('it-pipe')
+
+const createNode = async () => {
+  const peerInfo = await PeerInfo.create()
+  peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
+
+  const node = await Libp2p.create({
+    peerInfo,
+    modules: {
+      transport: [TCP],
+      streamMuxer: [Mplex],
+      connEncryption: [SECIO]
+    }
+  })
+
+  await node.start()
+
+  return node
+}
+
+;(async () => {
+  const [node1, node2] = await Promise.all([
+    createNode(),
+    createNode()
+  ])
+
+  node2.handle('/a-protocol', ({ stream }) => {
+    pipe(
+      stream,
+      async function (source) {
+        for await (const msg of source) {
+          console.log(msg.toString())
+        }
       }
-    }
-
-    super(defaultsDeep(_options, defaults))
-  }
-}
-
-function createNode (callback) {
-  let node
-
-  waterfall([
-    (cb) => PeerInfo.create(cb),
-    (peerInfo, cb) => {
-      peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
-      node = new MyBundle({
-        peerInfo
-      })
-      node.start(cb)
-    }
-  ], (err) => callback(err, node))
-}
-
-parallel([
-  (cb) => createNode(cb),
-  (cb) => createNode(cb)
-], (err, nodes) => {
-  if (err) { throw err }
-
-  const node1 = nodes[0]
-  const node2 = nodes[1]
-
-  node2.handle('/a-protocol', (protocol, conn) => {
-    pull(
-      conn,
-      pull.map((v) => v.toString()),
-      pull.log()
     )
   })
 
-  node1.dialProtocol(node2.peerInfo, '/a-protocol', (err, conn) => {
-    if (err) { throw err }
-    pull(pull.values(['This information is sent out encrypted to the other peer']), conn)
-  })
-})
+  const { stream } = await node1.dialProtocol(node2.peerInfo, '/a-protocol')
+
+  await pipe(
+    ['This information is sent out encrypted to the other peer'],
+    stream
+  )
+})();
