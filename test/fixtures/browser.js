21a79a0e6b48a1f788747fd9d05f886d05a75cbe
Vasco Santos <vasco.santos@moxy.studio>
2020-06-08 18:27:31 +0200

    chore: use ed25519 keys in tests

--- test/fixtures/browser.js
+++ test/fixtures/browser.js
@@ -3,5 +3,5 @@
 const multiaddr = require('multiaddr')
 
 module.exports.MULTIADDRS_WEBSOCKETS = [
-  multiaddr('/ip4/127.0.0.1/tcp/15001/ws/p2p/QmckxVrJw1Yo8LqvmDJNUmdAsKtSbiKWmrXJFyKmUraBoN')
+multiaddr('/ip4/127.0.0.1/tcp/15001/ws/p2p/12D3KooWHFKTMzwerBtsVmtz4ZZEQy2heafxzWw6wNn5PPYkBxJ5')
 ]
