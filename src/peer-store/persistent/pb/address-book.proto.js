305a9eda96c541ef8c0ce1522396d2c452689bed
Vasco Santos <vasco.santos@moxy.studio>
2020-06-21 19:33:58 +0200

    feat: cerified addressbook

--- src/peer-store/persistent/pb/address-book.proto.js
+++ src/peer-store/persistent/pb/address-book.proto.js
@@ -4,11 +4,26 @@ const protons = require('protons')
 
 const message = `
 message Addresses {
+  // Address represents a single multiaddr.
   message Address {
     required bytes multiaddr = 1;
   }
 
+  // CertifiedRecord contains a serialized signed PeerRecord used to
+  // populate the signedAddrs list.
+  message CertifiedRecord {
+    // The Seq counter from the signed PeerRecord envelope
+    uint64 seq = 1;
+
+    // The serialized bytes of the SignedEnvelope containing the PeerRecord.
+    bytes raw = 2;
+  }
+
+  // The known multiaddrs.
   repeated Address addrs = 1;
+
+  // The most recently received signed PeerRecord.
+  CertifiedRecord certified_record = 2;
 }
 `
 
