d67190ba5809d330e6acb19b309cfed4d803cf8f
Vasco Santos <vasco.santos@moxy.studio>
2020-07-15 15:47:45 +0200

    fix: revert new identify protocol versions

--- src/identify/consts.js
+++ src/identify/consts.js
@@ -2,9 +2,5 @@
 
 module.exports.PROTOCOL_VERSION = 'ipfs/0.1.0'
 module.exports.AGENT_VERSION = 'js-libp2p/0.1.0'
-module.exports.MULTICODEC_IDENTIFY = '/p2p/id/1.1.0'
-module.exports.MULTICODEC_IDENTIFY_PUSH = '/p2p/id/push/1.1.0'
-
-// Legacy
-module.exports.MULTICODEC_IDENTIFY_1_0_0 = '/ipfs/id/1.0.0'
-module.exports.MULTICODEC_IDENTIFY_PUSH_1_0_0 = '/ipfs/id/push/1.0.0'
+module.exports.MULTICODEC_IDENTIFY = '/ipfs/id/1.0.0'
+module.exports.MULTICODEC_IDENTIFY_PUSH = '/ipfs/id/push/1.0.0'
