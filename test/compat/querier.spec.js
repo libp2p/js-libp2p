3cf0e75e3f9813e70fe4449d266e709de8e472d7
Alex Potsides <alex@achingbrain.net>
2020-08-11 14:37:33 +0100

    chore: upgrade deps (#97)
    * chore: upgrade deps
    
    BREAKING CHANGES:
    
    - All deps of this module now use Uint8Arrays instead of node Buffers
    
    * chore: address pr comments

--- test/compat/querier.spec.js
+++ test/compat/querier.spec.js
@@ -1,10 +1,7 @@
 /* eslint-env mocha */
 'use strict'
 
-const chai = require('chai')
-const dirtyChai = require('dirty-chai')
-const expect = chai.expect
-chai.use(dirtyChai)
+const { expect } = require('aegir/utils/chai')
 const PeerId = require('peer-id')
 const MDNS = require('multicast-dns')
 const OS = require('os')
