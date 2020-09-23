87d20ac46dbe242455ba8ccbc81ee99109dca485
Vasco Santos <vasco.santos@moxy.studio>
2020-09-23 18:45:01 +0200

    chore: create signed peer record on new listen addresses in transport manager

--- test/transports/transport-manager.node.js
+++ test/transports/transport-manager.node.js
@@ -4,12 +4,16 @@
 const chai = require('chai')
 chai.use(require('dirty-chai'))
 const { expect } = chai
+const sinon = require('sinon')
 
 const AddressManager = require('../../src/address-manager')
 const TransportManager = require('../../src/transport-manager')
+const PeerStore = require('../../src/peer-store')
 const Transport = require('libp2p-tcp')
+const PeerId = require('peer-id')
 const multiaddr = require('multiaddr')
 const mockUpgrader = require('../utils/mockUpgrader')
+const Peers = require('../fixtures/peers')
 const addrs = [
   multiaddr('/ip4/127.0.0.1/tcp/0'),
   multiaddr('/ip4/127.0.0.1/tcp/0')
@@ -17,11 +21,17 @@ const addrs = [
 
 describe('Transport Manager (TCP)', () => {
   let tm
+  let localPeer
+
+  before(async () => {
+    localPeer = await PeerId.createFromJSON(Peers[0])
+  })
 
   before(() => {
     tm = new TransportManager({
       libp2p: {
-        addressManager: new AddressManager({ listen: addrs })
+        addressManager: new AddressManager({ listen: addrs }),
+        PeerStore: new PeerStore({ peerId: localPeer })
       },
       upgrader: mockUpgrader,
       onConnection: () => {}
@@ -40,10 +50,16 @@ describe('Transport Manager (TCP)', () => {
   })
 
   it('should be able to listen', async () => {
+    sinon.spy(tm, '_createSelfPeerRecord')
+
     tm.add(Transport.prototype[Symbol.toStringTag], Transport)
     await tm.listen(addrs)
     expect(tm._listeners).to.have.key(Transport.prototype[Symbol.toStringTag])
     expect(tm._listeners.get(Transport.prototype[Symbol.toStringTag])).to.have.length(addrs.length)
+
+    // Created Self Peer record on new listen address
+    expect(tm._createSelfPeerRecord.callCount).to.equal(addrs.length)
+
     // Ephemeral ip addresses may result in multiple listeners
     expect(tm.getAddrs().length).to.equal(addrs.length)
     await tm.close()
