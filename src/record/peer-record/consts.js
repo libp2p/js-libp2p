01d43a7b60f73061205c39352ae38d27ea8c072a
Vasco Santos <vasco.santos@moxy.studio>
2020-12-15 14:54:38 +0100

    chore: fix multicodec updates (#835)
    * chore: fix specific multicodec version
    
    * chore: fix multicodec issues
    
    * chore: remove prepare script

--- src/record/peer-record/consts.js
+++ src/record/peer-record/consts.js
@@ -3,9 +3,14 @@
 const multicodec = require('multicodec')
 
 // The domain string used for peer records contained in a Envelope.
-module.exports.ENVELOPE_DOMAIN_PEER_RECORD = multicodec.getName(multicodec.LIBP2P_PEER_RECORD)
+const domain = multicodec.getName(multicodec.LIBP2P_PEER_RECORD) || 'libp2p-peer-record'
 
 // The type hint used to identify peer records in a Envelope.
 // Defined in https://github.com/multiformats/multicodec/blob/master/table.csv
 // with name "libp2p-peer-record"
-module.exports.ENVELOPE_PAYLOAD_TYPE_PEER_RECORD = Uint8Array.from([3, 1])
+const payloadType = Uint8Array.from([3, 1])
+
+module.exports = {
+  ENVELOPE_DOMAIN_PEER_RECORD: domain,
+  ENVELOPE_PAYLOAD_TYPE_PEER_RECORD: payloadType
+}
