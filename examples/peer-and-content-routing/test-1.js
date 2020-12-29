8e104699c57f8513c33b4bd0b9bb380803a89dae
Vasco Santos <vasco.santos@moxy.studio>
2020-12-29 11:38:56 +0000

    chore: add peer and content routing example tests

---
+++ examples/peer-and-content-routing/test-1.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const path = require('path')
+const execa = require('execa')
+const pWaitFor = require('p-wait-for')
+const uint8ArrayToString = require('uint8arrays/to-string')
+
+async function test() {
+  process.stdout.write('1.js\n')
+
+  const addrs = []
+  let foundIt = false
+  const proc = execa('node', [path.join(__dirname, '1.js')], {
+    cwd: path.resolve(__dirname),
+    all: true
+  })
+
+  proc.all.on('data', async (data) => {
+    process.stdout.write(data)
+
+    const line = uint8ArrayToString(data)
+
+    // Discovered peer
+    if (!foundIt && line.includes('Found it, multiaddrs are:')) {
+      foundIt = true
+    }
+
+    addrs.push(line)
+  })
+
+  await pWaitFor(() => addrs.length === 2)
+
+  proc.kill()
+}
+
+module.exports = test
---
+++ examples/peer-and-content-routing/test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const test1 = require('./test-1')
+const test2 = require('./test-2')
+
+async function test() {
+  await test1()
+  await test2()
+}
+
+module.exports = test
