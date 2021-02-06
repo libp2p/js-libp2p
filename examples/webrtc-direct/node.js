76e49150133f374fa276ecbf0254486e5cbd95d4
vogdb <vozhdb@gmail.com>
2021-02-06 19:10:41 +0100

    use libp2p

--- examples/webrtc-direct/node.js
+++ examples/webrtc-direct/node.js
@@ -1,15 +1,12 @@
 const Libp2p = require('libp2p')
-const IPFS = require('ipfs')
 const Bootstrap = require('libp2p-bootstrap')
 const WebRTCDirect = require('libp2p-webrtc-direct')
 const Mplex = require('libp2p-mplex')
 const {NOISE} = require('libp2p-noise')
 
-const libp2pBundle = (opts) => {
-  const peerId = opts.peerId
+;(async () => {
 
-  return new Libp2p({
-    peerId,
+  const node =  await Libp2p.create({
     addresses: {
       listen: ['/ip4/127.0.0.1/tcp/9090/http/p2p-webrtc-direct']
     },
@@ -26,27 +23,14 @@ const libp2pBundle = (opts) => {
       }
     }
   })
-}
 
-async function main () {
-  const node = await IPFS.create({
-    repo: 'ipfs-' + Math.random(),
-    libp2p: libp2pBundle
+  node.connectionManager.on('peer:connect', (connection) => {
+    console.info(`Connected to ${connection.remotePeer.toB58String()}!`)
   })
-  console.log(`ipfs node's config: ${JSON.stringify(await node.id())}`)
 
-  const interval = setInterval(async () => {
-    try {
-      const peers = await node.swarm.peers()
-      if (peers.length > 0) {
-        console.log(`The node now has ${peers.length} peers.`)
-        console.log(`peers: ${JSON.stringify(peers, null, 2)}`)
-        clearInterval(interval)
-      }
-    } catch (err) {
-      console.log('An error occurred trying to check out peers:', err)
-    }
-  }, 2000)
-}
+  await node.start()
 
-main()
+  console.log('Listening on:')
+  node.multiaddrs.forEach((ma) => console.log(`${ma.toString()}/p2p/${node.peerId.toB58String()}`))
+
+})()
--- examples/webrtc-direct/public/browser.js
+++ examples/webrtc-direct/public/browser.js
@@ -1,15 +1,11 @@
 const Libp2p = require('libp2p')
-const IPFS = require('ipfs')
 const WebRTCDirect = require('libp2p-webrtc-direct')
 const Mplex = require('libp2p-mplex')
 const {NOISE} = require('libp2p-noise')
 const Bootstrap = require('libp2p-bootstrap')
 
-const libp2pBundle = (opts) => {
-  const peerId = opts.peerId
-
-  return new Libp2p({
-    peerId,
+document.addEventListener('DOMContentLoaded', async () => {
+  const node = await Libp2p.create({
     modules: {
       transport: [WebRTCDirect],
       streamMuxer: [Mplex],
@@ -20,37 +16,27 @@ const libp2pBundle = (opts) => {
       peerDiscovery: {
         [Bootstrap.tag]: {
           enabled: true,
-          list: ['/ip4/127.0.0.1/tcp/9090/http/p2p-webrtc-direct/p2p/QmTVapMVSELAcdxydUdtD5215vhqCxbsRjxYfmcLPabhk7']
+          list: ['/ip4/127.0.0.1/tcp/9090/http/p2p-webrtc-direct/p2p/QmS4W12SAA4yLxy4YwhQaJZqY82wyYLqJjhuZYSe5f2SKj']
         }
       }
     }
   })
-}
 
 
-async function main () {
-  const node = await IPFS.create({
-    repo: 'ipfs-' + Math.random(),
-    libp2p: libp2pBundle
-    // How to correctly set bootstrappers?
-    // libp2p: (...args) => createLibp2p(...args, ['/ip4/127.0.0.1/tcp/9090/http/p2p-webrtc-direct/p2p/QmYdb3fr52UxH6evf9UnKY8YvvRrnskt9ULKuS2n69THz9']),
-    // Bootstrap: ['/ip4/127.0.0.1/tcp/9090/http/p2p-webrtc-direct/p2p/QmYdb3fr52UxH6evf9UnKY8YvvRrnskt9ULKuS2n69THz9']
+  // Listen for new peers
+  node.on('peer:discovery', (peerId) => {
+    console.log(`Found peer ${peerId.toB58String()}`)
   })
 
-  const interval = setInterval(async () => {
-    try {
-      const peers = await node.swarm.peers()
-      if (peers.length > 0) {
-        console.log(`The node now has ${peers.length} peers.`)
-        console.log(`peers: ${JSON.stringify(peers, null, 2)}`)
-        clearInterval(interval)
-      }
-    } catch (err) {
-      console.log('An error occurred trying to check out peers:', err)
-    }
-  }, 2000)
-}
+  // Listen for new connections to peers
+  node.connectionManager.on('peer:connect', (connection) => {
+    console.log(`Connected to ${connection.remotePeer.toB58String()}`)
+  })
 
-document.addEventListener('DOMContentLoaded', async () => {
-  main()
+  // Listen for peers disconnecting
+  node.connectionManager.on('peer:disconnect', (connection) => {
+    console.log(`Disconnected from ${connection.remotePeer.toB58String()}`)
+  })
+
+  await node.start()
 })
