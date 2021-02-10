b1079474de108d383913992e0b5d55ff9b2ae275
Vasco Santos <vasco.santos@moxy.studio>
2021-02-10 15:40:19 +0100

    chore: add protocol and stream muxing example tests (#849)

---
+++ examples/protocol-and-stream-muxing/test-1.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const path = require('path')
+const execa = require('execa')
+const pDefer = require('p-defer')
+const uint8ArrayToString = require('uint8arrays/to-string')
+
+async function test() {
+  const messageDefer = pDefer()
+  process.stdout.write('1.js\n')
+
+  const proc = execa('node', [path.join(__dirname, '1.js')], {
+    cwd: path.resolve(__dirname),
+    all: true
+  })
+
+  proc.all.on('data', async (data) => {
+    process.stdout.write(data)
+
+    const line = uint8ArrayToString(data)
+
+    if (line.includes('my own protocol, wow!')) {
+      messageDefer.resolve()
+    }
+  })
+
+  await messageDefer.promise
+  proc.kill()
+}
+
+module.exports = test
---
+++ examples/protocol-and-stream-muxing/test-3.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const path = require('path')
+const execa = require('execa')
+const pWaitFor = require('p-wait-for')
+const uint8ArrayToString = require('uint8arrays/to-string')
+
+const messages = [
+  'from 1 to 2',
+  'from 2 to 1'
+]
+
+async function test() {
+  process.stdout.write('3.js\n')
+
+  let count = 0
+  const proc = execa('node', [path.join(__dirname, '3.js')], {
+    cwd: path.resolve(__dirname),
+    all: true
+  })
+
+  proc.all.on('data', async (data) => {
+    process.stdout.write(data)
+
+    const line = uint8ArrayToString(data)
+
+    if (messages.find((m) => line.includes(m))) {
+      count += 1
+    }
+  })
+
+  await pWaitFor(() => count === messages.length)
+
+  proc.kill()
+}
+
+module.exports = test
