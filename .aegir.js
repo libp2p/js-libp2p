ff4b764eaf8fac8ac04f48b0769a9eeb8297f9a5
Vasco Santos <vasco.santos@moxy.studio>
2021-03-31 16:04:13 +0200

    chore: update deps (#123)

--- .aegir.js
+++ .aegir.js
@@ -10,7 +10,7 @@ const mockUpgrader = {
 }
 let listener
 
-async function boot () {
+async function before () {
   const ws = new WS({ upgrader: mockUpgrader })
   const ma = multiaddr('/ip4/127.0.0.1/tcp/9095/ws')
   listener = ws.createListener(conn => pipe(conn, conn))
@@ -18,15 +18,13 @@ async function boot () {
   listener.on('error', console.error)
 }
 
-function shutdown () {
+function after () {
   return listener.close()
 }
 
 module.exports = {
-  hooks: {
-    browser: {
-      pre: boot,
-      post: shutdown
-    }
+  test: {
+    before,
+    after
   }
 }
