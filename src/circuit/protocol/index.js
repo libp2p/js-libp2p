b043bca607565cf534771e6cf975288a8ff3030b
Vasco Santos <vasco.santos@moxy.studio>
2021-04-20 16:25:48 +0200

    fix: specify pbjs root (#917)

--- src/circuit/protocol/index.js
+++ src/circuit/protocol/index.js
@@ -7,7 +7,7 @@ var $protobuf = require("protobufjs/minimal");
 var $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
 
 // Exported root namespace
-var $root = $protobuf.roots["default"] || ($protobuf.roots["default"] = {});
+var $root = $protobuf.roots["libp2p-circuit"] || ($protobuf.roots["libp2p-circuit"] = {});
 
 $root.CircuitRelay = (function() {
 
--- src/identify/message.js
+++ src/identify/message.js
@@ -7,7 +7,7 @@ var $protobuf = require("protobufjs/minimal");
 var $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
 
 // Exported root namespace
-var $root = $protobuf.roots["default"] || ($protobuf.roots["default"] = {});
+var $root = $protobuf.roots["libp2p-identify"] || ($protobuf.roots["libp2p-identify"] = {});
 
 $root.Identify = (function() {
 
