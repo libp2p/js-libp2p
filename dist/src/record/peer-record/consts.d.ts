0c8b2dc0640430abf3c3e9ba3c622b7f6896665a
Vasco Santos <vasco.santos@moxy.studio>
2021-04-29 18:53:10 +0200

    chore: remove dist

--- dist/src/record/peer-record/consts.d.ts
+++
@@ -1,5 +0,0 @@
-declare const domain: multicodec.CodecName;
-declare const payloadType: Uint8Array;
-import multicodec = require("multicodec");
-export { domain as ENVELOPE_DOMAIN_PEER_RECORD, payloadType as ENVELOPE_PAYLOAD_TYPE_PEER_RECORD };
-//# sourceMappingURL=consts.d.ts.map
