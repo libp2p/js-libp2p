725c54393ecb154c9a1f864a069e7cf66d02945e
achingbrain <alex@achingbrain.net>
2021-11-19 08:04:24 +0000

    chore: revert change

--- src/peer-store/address-book.js
+++ src/peer-store/address-book.js
@@ -223,7 +223,7 @@ class AddressBook extends Book {
   add (peerId, multiaddrs) {
     if (!PeerId.isPeerId(peerId)) {
       log.error('peerId must be an instance of peer-id to store data')
-      throw errcode(new Error('peerId must be an instance of peer-id ' + peerId), ERR_INVALID_PARAMETERS)
+      throw errcode(new Error('peerId must be an instance of peer-id'), ERR_INVALID_PARAMETERS)
     }
 
     const addresses = this._toAddresses(multiaddrs)
