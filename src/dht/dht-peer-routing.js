d5c5777df948cb00f016ee3a82830a652cbc246b
Robert Kiel <robert.kiel@hoprnet.io>
2021-12-20 22:22:42 +0100

    proper findPeer call

--- src/dht/dht-peer-routing.js
+++ src/dht/dht-peer-routing.js
@@ -6,6 +6,7 @@ const { messages, codes } = require('../errors')
 /**
  * @typedef {import('peer-id')} PeerId
  * @typedef {import('libp2p-interfaces/src/peer-routing/types').PeerRouting} PeerRoutingModule
+ * @typedef {import('libp2p-kad-dht/dist/src/types').PeerData} PeerData
  */
 
 /**
@@ -26,20 +27,53 @@ class DHTPeerRouting {
    * @param {any} options
    */
   async findPeer (peerId, options = {}) {
+    /** @type {PeerData} */
+    const result = {
+      id: peerId,
+      multiaddrs: []
+    }
+    let peerFound = false
     for await (const event of this._dht.findPeer(peerId, options)) {
+      let multiaddrs 
       switch (event.name) {
         case 'FINAL_PEER':
-          return event.peer
+          peerFound = true
+          multiaddrs = event.peer.multiaddrs
+          break
         case 'PEER_RESPONSE':
+          peerFound = true
           const peer = event.closer.find(peerData => peerData.id.equals(peerId))
 
           if (peer) {
-            return peer
+            multiaddrs = peer.multiaddrs
           }
           break
         default:
           continue
       }
+
+      if (multiaddrs && multiaddrs.length > 0) {
+        for (const multiaddr of multiaddrs) {
+          let alreadyKnown = false
+          for (const existing of result.multiaddrs) {
+            if (existing.equals(multiaddr)) {
+              alreadyKnown = true
+            }
+          }
+          if (!alreadyKnown) {
+            result.multiaddrs.push(multiaddr)
+          }
+        }
+      }
+
+      if (event.name === 'FINAL_PEER') {
+        return result
+      }
+     
+    }
+
+    if (peerFound) {
+      return result
     }
 
     throw errCode(new Error(messages.NOT_FOUND), codes.ERR_NOT_FOUND)
