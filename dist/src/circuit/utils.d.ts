4ce61db971e78ba13574a8db74e7cf991542dc87
Robert Kiel <robert.kiel@hoprnet.io>
2022-01-04 09:00:12 +0100

    cleanup

--- dist/src/circuit/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export function namespaceToCid(namespace: string): Promise<CID>;
-import { CID } from "multiformats/cid";
-//# sourceMappingURL=utils.d.ts.map
--- dist/src/ping/util.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @param {number} length
- */
-export function rnd(length: number): Uint8Array;
-//# sourceMappingURL=util.d.ts.map
