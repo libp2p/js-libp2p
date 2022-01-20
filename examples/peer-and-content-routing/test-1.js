9dbbc30a16d67ff8470974ba21d45052a8671c49
achingbrain <alex@achingbrain.net>
2022-01-20 07:04:22 +0000

    chore: simplify test

--- examples/peer-and-content-routing/test-1.js
+++ examples/peer-and-content-routing/test-1.js
@@ -2,36 +2,28 @@
 
 const path = require('path')
 const execa = require('execa')
-const pWaitFor = require('p-wait-for')
 const { toString: uint8ArrayToString } = require('uint8arrays/to-string')
 
 async function test() {
   process.stdout.write('1.js\n')
 
-  const addrs = []
-  let foundIt = false
   const proc = execa('node', [path.join(__dirname, '1.js')], {
     cwd: path.resolve(__dirname),
     all: true
   })
 
+  let contents = ''
+
   proc.all.on('data', async (data) => {
     process.stdout.write(data)
 
-    const line = uint8ArrayToString(data)
+    contents += uint8ArrayToString(data)
 
     // Discovered peer
-    if (!foundIt && line.includes('Found it, multiaddrs are:')) {
-      foundIt = true
+    if (contents.includes('Found it, multiaddrs are:')) {
+      proc.kill()
     }
-
-    addrs.push(line)
   })
-
-  await pWaitFor(() => addrs.length === 2)
-
-  process.stdout.write('kill process\n')
-  proc.kill()
 }
 
 module.exports = test
