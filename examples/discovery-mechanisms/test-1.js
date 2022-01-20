ab6090c7085b85e2a09b516afc824d2f40e2dd6d
achingbrain <alex@achingbrain.net>
2022-01-20 13:13:54 +0000

    chore: update example tests
    Remove some of the redundancy in the example tests

--- examples/discovery-mechanisms/test-1.js
+++ examples/discovery-mechanisms/test-1.js
@@ -1,27 +1,13 @@
 'use strict'
 
 const path = require('path')
-const execa = require('execa')
-const { toString: uint8ArrayToString } = require('uint8arrays/to-string')
+const { waitForOutput } = require('../utils')
 
 async function test () {
   process.stdout.write('1.js\n')
 
-  const proc = execa('node', [path.join(__dirname, '1.js')], {
-    cwd: path.resolve(__dirname),
-    all: true
-  })
-
-  let output = ''
-
-  proc.all.on('data', async (data) => {
-    process.stdout.write(data)
-    output += uint8ArrayToString(data)
-
-    // Discovered and connected
-    if (output.includes('Connection established to:')) {
-      proc.kill()
-    }
+  await waitForOutput('Connection established to:', 'node', [path.join(__dirname, '1.js')], {
+    cwd: __dirname
   })
 }
 
--- examples/peer-and-content-routing/test-2.js
+++ examples/peer-and-content-routing/test-2.js
@@ -1,27 +1,13 @@
 'use strict'
 
 const path = require('path')
-const execa = require('execa')
-const { toString: uint8ArrayToString } = require('uint8arrays/to-string')
+const { waitForOutput } = require('../utils')
 
-async function test() {
+async function test () {
   process.stdout.write('2.js\n')
 
-  const proc = execa('node', [path.join(__dirname, '2.js')], {
-    cwd: path.resolve(__dirname),
-    all: true
-  })
-
-  let output = ''
-
-  proc.all.on('data', async (data) => {
-    process.stdout.write(data)
-
-    output += uint8ArrayToString(data)
-
-    if (output.includes('Found provider:')) {
-      proc.kill()
-    }
+  await waitForOutput('Found provider:', 'node', [path.join(__dirname, '2.js')], {
+    cwd: __dirname
   })
 }
 
