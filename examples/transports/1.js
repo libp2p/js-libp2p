384166b38d008f878e5eaf68ce0c0fbc4843a187
Lindsay jo Walker <lindsayjowalker@lindsays-mbp.lan>
2022-04-28 15:18:19 +0200

    updated 1.js example

--- examples/transports/1.js
+++ examples/transports/1.js
@@ -1,34 +1,32 @@
-/* eslint-disable no-console */
+'use strict'
 
-import { createLibp2p } from 'libp2p'
-import { TCP } from '@libp2p/tcp'
-import { Noise } from '@chainsafe/libp2p-noise'
+const Libp2p = require('libp2p')
+const TCP = require('libp2p-tcp')
+const { NOISE } = require('@chainsafe/libp2p-noise')
 
 const createNode = async () => {
-  const node = await createLibp2p({
+  const node = await Libp2p.create({
     addresses: {
       // To signal the addresses we want to be available, we use
       // the multiaddr format, a self describable address
-      listen: [
-        '/ip4/0.0.0.0/tcp/0'
-      ]
+      listen: ['/ip4/0.0.0.0/tcp/0']
     },
-    transports: [
-      new TCP()
-    ],
-    connectionEncryption: [
-      new Noise()
-    ]
+    modules: {
+      transport: [TCP],
+      connEncryption: [NOISE]
+    }
   })
 
   await node.start()
-  return node
-}
-
-;(async () => {
-  const node = await createNode()
 
   console.log('node has started (true/false):', node.isStarted())
   console.log('listening on:')
-  node.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
-})();
+  node.multiaddrs.forEach((ma) => console.log(`${ma.toString()}/p2p/${node.peerId.toB58String()}`))
+
+  return node
+}
+
+createNode().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
