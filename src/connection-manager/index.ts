46904245e1886470a561652b7d174e13e30d3e43
achingbrain <alex@achingbrain.net>
2022-05-23 13:33:07 +0100

    fix: close streams
    Updates all deps to close multiplexed streams when closing connections.

--- src/connection-manager/index.ts
+++ src/connection-manager/index.ts
@@ -10,7 +10,6 @@ import type { Startable } from '@libp2p/interfaces/startable'
 import { trackedMap } from '@libp2p/tracked-map'
 import { codes } from '../errors.js'
 import { isPeerId, PeerId } from '@libp2p/interfaces/peer-id'
-// @ts-expect-error setMaxListeners is missing from the node 16 types
 import { setMaxListeners } from 'events'
 import type { Connection } from '@libp2p/interfaces/connection'
 import type { ConnectionManager } from '@libp2p/interfaces/connection-manager'
