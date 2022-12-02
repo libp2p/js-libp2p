584c6fadf9d7a0805d33edc86a8a2aa5a87901a3
David DiMaria <info@collectivesessions.com>
2022-12-02 08:16:02 -0700

    Refactor stream tests

--- test/stream.browser.spec.ts
+++ test/stream.browser.spec.ts
@@ -1,74 +1,68 @@
 import * as underTest from '../src/stream'
 import { expect, assert } from 'chai'
 
+function setup (): { peerConnection: RTCPeerConnection, datachannel: RTCDataChannel, webrtcStream: underTest.WebRTCStream } {
+  const peerConnection = new RTCPeerConnection()
+  const datachannel = peerConnection.createDataChannel('whatever', { negotiated: true, id: 91 })
+  const webrtcStream = new underTest.WebRTCStream({ channel: datachannel, stat: underTest.defaultStat('outbound') })
+
+  return { peerConnection, datachannel, webrtcStream }
+}
+
 describe('Stream Stats', () => {
   it('can construct', () => {
-    const pc = new RTCPeerConnection()
-    const dc = pc.createDataChannel('whatever', { negotiated: true, id: 91 })
-    const s = new underTest.WebRTCStream({ channel: dc, stat: underTest.defaultStat('outbound') })
-    // expect(s.stat.timeline.close).to.not.exist();
-    assert.notExists(s.stat.timeline.close)
+    const { webrtcStream } = setup()
+    assert.notExists(webrtcStream.stat.timeline.close)
   })
 
   it('close marks it closed', () => {
-    const pc = new RTCPeerConnection()
-    const dc = pc.createDataChannel('whatever', { negotiated: true, id: 91 })
-    const s = new underTest.WebRTCStream({ channel: dc, stat: underTest.defaultStat('outbound') })
+    const { webrtcStream } = setup()
 
-    expect(s.streamState.state).to.equal(underTest.StreamStates.OPEN)
-    s.close()
-    expect(s.streamState.state).to.equal(underTest.StreamStates.CLOSED)
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.OPEN)
+    webrtcStream.close()
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.CLOSED)
   })
 
   it('closeRead marks it read-closed only', () => {
-    const pc = new RTCPeerConnection()
-    const dc = pc.createDataChannel('whatever', { negotiated: true, id: 91 })
-    const s = new underTest.WebRTCStream({ channel: dc, stat: underTest.defaultStat('outbound') })
-    expect(s.streamState.state).to.equal(underTest.StreamStates.OPEN)
-    s.closeRead()
-    expect(s.streamState.state).to.equal(underTest.StreamStates.READ_CLOSED)
+    const { webrtcStream } = setup()
+
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.OPEN)
+    webrtcStream.closeRead()
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.READ_CLOSED)
   })
 
   it('closeWrite marks it write-closed only', () => {
-    const pc = new RTCPeerConnection()
-    const dc = pc.createDataChannel('whatever', { negotiated: true, id: 91 })
-    const s = new underTest.WebRTCStream({ channel: dc, stat: underTest.defaultStat('outbound') })
-    expect(s.streamState.state).to.equal(underTest.StreamStates.OPEN)
-    s.closeWrite()
-    expect(s.streamState.state).to.equal(underTest.StreamStates.WRITE_CLOSED)
+    const { webrtcStream } = setup()
+
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.OPEN)
+    webrtcStream.closeWrite()
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.WRITE_CLOSED)
   })
 
   it('closeWrite AND closeRead = close', () => {
-    const pc = new RTCPeerConnection()
-    const dc = pc.createDataChannel('whatever', { negotiated: true, id: 91 })
-    const s = new underTest.WebRTCStream({ channel: dc, stat: underTest.defaultStat('outbound') })
-    s.closeWrite()
-    expect(s.streamState.state).to.equal(underTest.StreamStates.WRITE_CLOSED)
-    s.closeRead()
-    expect(s.streamState.state).to.equal(underTest.StreamStates.CLOSED)
+    const { webrtcStream } = setup()
+
+    webrtcStream.closeWrite()
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.WRITE_CLOSED)
+    webrtcStream.closeRead()
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.CLOSED)
   })
 
   it('abort = close', () => {
-    const pc = new RTCPeerConnection()
-    const dc = pc.createDataChannel('whatever', { negotiated: true, id: 91 })
-    const s = new underTest.WebRTCStream({ channel: dc, stat: underTest.defaultStat('outbound') })
-    // expect(s.stat.timeline.close).to.not.exist();
-    expect(s.streamState.state).to.equal(underTest.StreamStates.OPEN)
-    s.abort({ name: 'irrelevant', message: 'this parameter is actually ignored' })
-    expect(s.streamState.state).to.equal(underTest.StreamStates.CLOSED)
-    // expect(s.stat.timeline.close).to.exist();
-    expect(s.stat.timeline.close).to.be.greaterThan(s.stat.timeline.open)
+    const { webrtcStream } = setup()
+
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.OPEN)
+    webrtcStream.abort({ name: 'irrelevant', message: 'this parameter is actually ignored' })
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.CLOSED)
+    expect(webrtcStream.stat.timeline.close).to.be.greaterThan(webrtcStream.stat.timeline.open)
   })
 
   it('reset = close', () => {
-    const pc = new RTCPeerConnection()
-    const dc = pc.createDataChannel('whatever', { negotiated: true, id: 91 })
-    const s = new underTest.WebRTCStream({ channel: dc, stat: underTest.defaultStat('outbound') })
-    // expect(s.stat.timeline.close).to.not.exist();
-    expect(s.streamState.state).to.equal(underTest.StreamStates.OPEN)
-    s.reset() // only resets the write side
-    expect(s.streamState.state).to.equal(underTest.StreamStates.CLOSED)
-    // expect(s.stat.timeline.close).to.not.exist();
-    expect(dc.readyState).to.be.oneOf(['closing', 'closed'])
+    const { datachannel, webrtcStream } = setup()
+
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.OPEN)
+    webrtcStream.reset() // only resets the write side
+    expect(webrtcStream.streamState.state).to.equal(underTest.StreamStates.CLOSED)
+    expect(datachannel.readyState).to.be.oneOf(['closing', 'closed'])
   })
 })
