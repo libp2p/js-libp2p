e1f8d9cac3e4fcb284f754dc1c53428d424eddf9
achingbrain <alex@achingbrain.net>
2022-12-08 13:09:07 +0000

    docs: update fetch and pnet docs
    The docs in these modules are out of date

--- src/pnet/key-generator.ts
+++ src/pnet/key-generator.ts
@@ -5,24 +5,19 @@ import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 /**
  * Generates a PSK that can be used in a libp2p-pnet private network
  *
- * @param {Uint8Array} bytes - An object to write the psk into
+ * @param {Uint8Array | NodeJS.WriteStream} bytes - An object to write the psk into
  * @returns {void}
  */
-export function generateKey (bytes: Uint8Array) {
+export function generateKey (bytes: Uint8Array | NodeJS.WriteStream) {
   const psk = uint8ArrayToString(randomBytes(KEY_LENGTH), 'base16')
   const key = uint8ArrayFromString('/key/swarm/psk/1.0.0/\n/base16/\n' + psk)
 
-  bytes.set(key)
+  if (bytes instanceof Uint8Array) {
+    bytes.set(key)
+  } else {
+    bytes.write(key)
+  }
 }
 
 export const NONCE_LENGTH = 24
 export const KEY_LENGTH = 32
-
-try {
-  if (require.main === module) {
-    // @ts-expect-error
-    generate(process.stdout)
-  }
-} catch (error: any) {
-
-}
