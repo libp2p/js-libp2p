ceb337401c5a23b437e5f057863a183c5f589149
achingbrain <alex@achingbrain.net>
2023-01-16 16:44:39 +0000

    chore: add test for key decryption failure

--- test/core/peer-id.spec.ts
+++ test/core/peer-id.spec.ts
@@ -15,7 +15,7 @@ describe('peer-id', () => {
     }
   })
 
-  it('it should create a PeerId if none is passed', async () => {
+  it('should create a PeerId if none is passed', async () => {
     libp2p = await createLibp2p({
       transports: [
         webSockets()
@@ -28,7 +28,7 @@ describe('peer-id', () => {
     expect(libp2p.peerId).to.be.ok()
   })
 
-  it('it should retrieve the PeerId from the datastore', async () => {
+  it('should retrieve the PeerId from the datastore', async () => {
     const datastore = new MemoryDatastore()
 
     libp2p = await createLibp2p({
@@ -62,7 +62,7 @@ describe('peer-id', () => {
     expect(libp2p.peerId.toString()).to.equal(peerId.toString())
   })
 
-  it('it should retrieve the PeerId from the datastore with a keychain password', async () => {
+  it('should retrieve the PeerId from the datastore with a keychain password', async () => {
     const datastore = new MemoryDatastore()
     const keychain = {
       pass: 'very-long-password-must-be-over-twenty-characters-long',
@@ -102,5 +102,44 @@ describe('peer-id', () => {
     // the new node should have read the PeerId from the datastore
     // instead of creating a new one
     expect(libp2p.peerId.toString()).to.equal(peerId.toString())
+  })
+
+  it('should fail to start if retrieving the PeerId from the datastore fails', async () => {
+    const datastore = new MemoryDatastore()
+    const keychain = {
+      pass: 'very-long-password-must-be-over-twenty-characters-long',
+      dek: {
+        salt: 'CpjNIxMqAZ+aJg+ezLfuzG4a'
+      }
+    }
+
+    libp2p = await createLibp2p({
+      datastore,
+      keychain,
+      transports: [
+        webSockets()
+      ],
+      connectionEncryption: [
+        plaintext()
+      ]
+    })
+    await libp2p.stop()
+
+    // creating a new node from the same datastore but with the wrong keychain config should fail
+    await expect(createLibp2p({
+      datastore,
+      keychain: {
+        pass: 'different-very-long-password-must-be-over-twenty-characters-long',
+        dek: {
+          salt: 'different-CpjNIxMqAZ+aJg+ezLfuzG4a'
+        }
+      },
+      transports: [
+        webSockets()
+      ],
+      connectionEncryption: [
+        plaintext()
+      ]
+    })).to.eventually.rejectedWith('Invalid PEM formatted message')
   })
 })
