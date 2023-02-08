e12202e9236c4cacb10938c1101563f4c7cf580c
Alex Potsides <alex@achingbrain.net>
2023-02-08 10:19:21 +0100

    fix: exclude peers without multiaddrs from auto dial (#1568)
    When the number of connections the node has drops below the low
    watermark and auto-dial begins, exclude peers without multiaddrs
    from the list of peers that the node tries to connect to.
    
    Also removes use of `it-pipe` to make it a little faster.

--- test/connection-manager/auto-dialler.spec.ts
+++ test/connection-manager/auto-dialler.spec.ts
@@ -8,6 +8,7 @@ import { createEd25519PeerId } from '@libp2p/peer-id-factory'
 import { stubInterface } from 'sinon-ts'
 import type { ConnectionManager } from '@libp2p/interface-connection-manager'
 import type { PeerStore, Peer } from '@libp2p/interface-peer-store'
+import { multiaddr } from '@multiformats/multiaddr'
 
 describe('Auto-dialler', () => {
   it('should not dial self', async () => {
@@ -23,7 +24,10 @@ describe('Auto-dialler', () => {
     const other: Peer = {
       id: await createEd25519PeerId(),
       protocols: [],
-      addresses: [],
+      addresses: [{
+        multiaddr: multiaddr('/ip4/127.0.0.1/tcp/4001'),
+        isCertified: true
+      }],
       metadata: new Map()
     }
 
@@ -53,5 +57,54 @@ describe('Auto-dialler', () => {
 
     expect(connectionManager.openConnection.callCount).to.equal(1)
     expect(connectionManager.openConnection.calledWith(self.id)).to.be.false()
+  })
+
+  it('should not dial peers without multiaddrs', async () => {
+    // peers with protocols are dialled before peers without protocols
+    const peerWithAddress: Peer = {
+      id: await createEd25519PeerId(),
+      protocols: [
+        '/foo/bar'
+      ],
+      addresses: [{
+        multiaddr: multiaddr('/ip4/127.0.0.1/tcp/4001'),
+        isCertified: true
+      }],
+      metadata: new Map()
+    }
+    const peerWithoutAddress: Peer = {
+      id: await createEd25519PeerId(),
+      protocols: [],
+      addresses: [],
+      metadata: new Map()
+    }
+
+    const peerStore = stubInterface<PeerStore>()
+
+    peerStore.all.returns(Promise.resolve([
+      peerWithAddress, peerWithoutAddress
+    ]))
+
+    const connectionManager = stubInterface<ConnectionManager>()
+    connectionManager.getConnections.returns([])
+
+    const autoDialler = new AutoDialler({
+      peerId: await createEd25519PeerId(),
+      peerStore,
+      connectionManager
+    }, {
+      minConnections: 10
+    })
+
+    await autoDialler.start()
+
+    await pWaitFor(() => connectionManager.openConnection.callCount === 1)
+    await delay(1000)
+
+    await autoDialler.stop()
+
+    expect(connectionManager.openConnection.callCount).to.equal(1)
+    expect(connectionManager.openConnection.calledWith(peerWithAddress.id)).to.be.true()
+    expect(connectionManager.openConnection.calledWith(peerWithoutAddress.id)).to.be.false()
   })
 })
