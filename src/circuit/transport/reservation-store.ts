c73635ff191bd707664416e508c22f146ccb8853
Chinmay Kousik <chinmaykousik1@gmail.com>
2023-03-21 14:05:55 +0530

    address review

--- src/circuit/transport/reservation-store.ts
+++ src/circuit/transport/reservation-store.ts
@@ -23,7 +23,7 @@ const REFRESH_WINDOW = (60 * 1000) * 10
 // try to refresh relay reservations 5 minutes before expiry
 const REFRESH_TIMEOUT = (60 * 1000) * 5
 
-// maximum duration before which a reservation should be refereshed
+// maximum duration before which a reservation should be refreshed
 const REFRESH_TIMEOUT_MAX = (60 * 1000) * 15
 // minimum duration before which a reservation must not be refreshed
 const REFRESH_TIMEOUT_MIN = 30 * 1000
@@ -169,7 +169,7 @@ export class ReservationStore extends EventEmitter<ReservationStoreEvents> imple
 
         const expiration = getExpirationMilliseconds(reservation.expire)
 
-        // sets a lower bound as 0 for the timeout
+        // sets a lower bound on the timeout
         const timeoutDuration = Math.max(expiration - REFRESH_TIMEOUT, REFRESH_TIMEOUT_MIN)
         // sets an upper bound on the timeout
         const boundedTimeoutDuration = Math.min(timeoutDuration, REFRESH_TIMEOUT_MAX)
