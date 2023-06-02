9c1cb8cc77bd1e65f0e7418ab469944456639258
Marco Munizaga <git@marcopolo.io>
2023-06-02 15:09:58 -0700

    eslint .aegir

--- interop/.aegir.js
+++ interop/.aegir.js
@@ -1,7 +1,7 @@
+import http from 'http'
 import { createClient } from 'redis'
-import http from "http"
 
-const redis_addr = process.env.redis_addr || 'redis:6379'
+const redisAddr = process.env.redis_addr || 'redis:6379'
 
 /** @type {import('aegir/types').PartialOptions} */
 export default {
@@ -12,29 +12,30 @@ export default {
         browserContextOptions: { ignoreHTTPSErrors: true }
       }
     },
-    async before() {
+    async before () {
       const redisClient = createClient({
-        url: `redis://${redis_addr}`
+        url: `redis://${redisAddr}`
       })
+      // eslint-disable-next-line no-console
       redisClient.on('error', (err) => console.error(`Redis Client Error: ${err}`))
       await redisClient.connect()
 
       const requestListener = async function (req, res) {
         const requestJSON = await new Promise(resolve => {
-          let body = ""
+          let body = ''
           req.on('data', function (data) {
-            body += data;
-          });
+            body += data
+          })
 
           req.on('end', function () {
             resolve(JSON.parse(body))
-          });
+          })
         })
 
         try {
           const redisRes = await redisClient.sendCommand(requestJSON)
           if (redisRes === null) {
-            throw new Error("redis sent back null")
+            throw new Error('redis sent back null')
           }
 
           res.writeHead(200, {
@@ -42,32 +43,30 @@ export default {
           })
           res.end(JSON.stringify(redisRes))
         } catch (err) {
-          console.error("Error in redis command:", err)
+          // eslint-disable-next-line no-console
+          console.error('Error in redis command:', err)
           res.writeHead(500, {
             'Access-Control-Allow-Origin': '*'
           })
           res.end(err.toString())
-          return
         }
+      }
 
-
-      };
-
-      const proxyServer = http.createServer(requestListener);
-      await new Promise(resolve => { proxyServer.listen(0, "localhost", () => { resolve() }); })
+      const proxyServer = http.createServer(requestListener)
+      await new Promise(resolve => { proxyServer.listen(0, 'localhost', () => { resolve() }) })
 
       return {
         redisClient,
-        proxyServer: proxyServer,
+        proxyServer,
         env: {
           ...process.env,
           proxyPort: proxyServer.address().port
         }
       }
     },
-    async after(_, { proxyServer, redisClient }) {
+    async after (_, { proxyServer, redisClient }) {
       await new Promise(resolve => {
-        proxyServer.close(() => resolve());
+        proxyServer.close(() => resolve())
       })
 
       try {
