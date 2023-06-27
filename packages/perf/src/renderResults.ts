9a7f9d835cb1b56d391cd7e527a4cd528ff1eee0
chad <chad.nehemiah94@gmail.com>
2023-06-27 15:29:10 -0500

    ci: updated job to generate report for performance data

---
+++ packages/perf/src/renderResults.ts
@@ -0,0 +1,55 @@
+interface TestResult {
+  previousUploadBandwidth: number;
+  currentUploadBandwidth: number;
+  previousDownloadBandwidth: number;
+  currentDownloadBandwidth: number;
+}
+
+interface TestEnvironment {
+  name: string;
+  results: TestResult;
+}
+
+export function generateMarkdownTable(environments: TestEnvironment[]): string {
+  const headerRow =
+    "| Environment | Previous Upload Bandwidth | Current Upload Bandwidth | Upload Bandwidth Comparison | Previous Download Bandwidth | Current Download Bandwidth | Download Bandwidth Comparison |\n";
+  const separatorRow =
+    "|-------------|--------------------------|-------------------------|-----------------------------|-----------------------------|----------------------------|-----------------------------|\n";
+  let bodyRows = "";
+
+  environments.forEach((environment) => {
+    const { name, results } = environment;
+    const {
+      previousUploadBandwidth,
+      currentUploadBandwidth,
+      previousDownloadBandwidth,
+      currentDownloadBandwidth,
+    } = results;
+
+    const uploadComparison = currentUploadBandwidth <= 0.8 * previousUploadBandwidth ? "❌" : "✅";
+    const downloadComparison = currentDownloadBandwidth <= 0.8 * previousDownloadBandwidth ? "❌" : "✅";
+
+    const row = `| ${name} | ${previousUploadBandwidth} | ${currentUploadBandwidth} ${uploadComparison} | ${previousDownloadBandwidth} | ${currentDownloadBandwidth} ${downloadComparison} |\n`;
+    bodyRows += row;
+  });
+
+  return `${headerRow}${separatorRow}${bodyRows}`;
+}
+
+const testEnvironments: TestEnvironment[] = [];
+
+const jsonObject = JSON.parse(process.env.BANDWIDTH_TEST_RESULTS ?? '');
+
+for (const name in jsonObject) {
+  if (Object.prototype.hasOwnProperty.call(jsonObject, name)) {
+    const testResult = jsonObject[name];
+    const testEnvironment: TestEnvironment = {
+      name: name,
+      results: testResult,
+    };
+    testEnvironments.push(testEnvironment);
+  }
+}
+
+const markdownTable = generateMarkdownTable(testEnvironments);
+console.log(markdownTable);
