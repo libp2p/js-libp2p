9bed517f337b4accd39cd40227f1ba033fecb188
chad <chad.nehemiah94@gmail.com>
2023-06-27 21:42:34 -0500

    chore: linting + updated ci job

--- packages/perf/src/renderResults.ts
+++ packages/perf/src/renderResults.ts
@@ -1,55 +1,51 @@
 interface TestResult {
-  previousUploadBandwidth: number;
-  currentUploadBandwidth: number;
-  previousDownloadBandwidth: number;
-  currentDownloadBandwidth: number;
+  previousUploadBandwidth: number
+  currentUploadBandwidth: number
+  previousDownloadBandwidth: number
+  currentDownloadBandwidth: number
 }
 
 interface TestEnvironment {
-  name: string;
-  results: TestResult;
+  name: string
+  results: TestResult
 }
 
-export function generateMarkdownTable(environments: TestEnvironment[]): string {
+export function generateMarkdownTable (environments: TestEnvironment[]): string {
   const headerRow =
-    "| Environment | Previous Upload Bandwidth | Current Upload Bandwidth | Upload Bandwidth Comparison | Previous Download Bandwidth | Current Download Bandwidth | Download Bandwidth Comparison |\n";
+    '| Environment | Previous Upload Bandwidth | Current Upload Bandwidth | Upload Bandwidth Comparison | Previous Download Bandwidth | Current Download Bandwidth | Download Bandwidth Comparison |\n'
   const separatorRow =
-    "|-------------|--------------------------|-------------------------|-----------------------------|-----------------------------|----------------------------|-----------------------------|\n";
-  let bodyRows = "";
+    '|-------------|--------------------------|-------------------------|-----------------------------|-----------------------------|----------------------------|-----------------------------|\n'
+  let bodyRows = ''
 
   environments.forEach((environment) => {
-    const { name, results } = environment;
-    const {
-      previousUploadBandwidth,
-      currentUploadBandwidth,
-      previousDownloadBandwidth,
-      currentDownloadBandwidth,
-    } = results;
+    const { name, results } = environment
+    const { previousUploadBandwidth, currentUploadBandwidth, previousDownloadBandwidth, currentDownloadBandwidth } = results
 
-    const uploadComparison = currentUploadBandwidth <= 0.8 * previousUploadBandwidth ? "❌" : "✅";
-    const downloadComparison = currentDownloadBandwidth <= 0.8 * previousDownloadBandwidth ? "❌" : "✅";
+    const uploadComparison = currentUploadBandwidth <= 0.8 * previousUploadBandwidth ? '❌' : '✅'
+    const downloadComparison = currentDownloadBandwidth <= 0.8 * previousDownloadBandwidth ? '❌' : '✅'
 
-    const row = `| ${name} | ${previousUploadBandwidth} | ${currentUploadBandwidth} ${uploadComparison} | ${previousDownloadBandwidth} | ${currentDownloadBandwidth} ${downloadComparison} |\n`;
-    bodyRows += row;
-  });
+    const row = `| ${name} | ${previousUploadBandwidth} | ${currentUploadBandwidth} ${uploadComparison} | ${previousDownloadBandwidth} | ${currentDownloadBandwidth} ${downloadComparison} |\n`
+    bodyRows += row
+  })
 
-  return `${headerRow}${separatorRow}${bodyRows}`;
+  return `${headerRow}${separatorRow}${bodyRows}`
 }
 
-const testEnvironments: TestEnvironment[] = [];
+const testEnvironments: TestEnvironment[] = []
 
-const jsonObject = JSON.parse(process.env.BANDWIDTH_TEST_RESULTS ?? '');
+const jsonObject = JSON.parse(process.env.BANDWIDTH_TEST_RESULTS ?? '')
 
 for (const name in jsonObject) {
   if (Object.prototype.hasOwnProperty.call(jsonObject, name)) {
-    const testResult = jsonObject[name];
+    const testResult = jsonObject[name]
     const testEnvironment: TestEnvironment = {
       name: name,
-      results: testResult,
-    };
-    testEnvironments.push(testEnvironment);
+      results: testResult
+    }
+    testEnvironments.push(testEnvironment)
   }
 }
 
-const markdownTable = generateMarkdownTable(testEnvironments);
-console.log(markdownTable);
+const markdownTable = generateMarkdownTable(testEnvironments)
+// eslint-disable-next-line no-console
+console.log(markdownTable)
