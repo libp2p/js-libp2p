cac61ff399e471f47ff276e16983a3c4e10b7bd3
achingbrain <alex@achingbrain.net>
2023-09-08 11:20:13 +0100

    fix(libp2p): sort addresses to dial as public, then relay
    Adds a new default address sorter that sorts by:
    
    1. public addresses
    2. public relay addresess
    3. private addresses
    4. private relay addresses
    
    Where they are equal, certified addresses take priority, otherwise
    the sort order is stable.

--- packages/utils/src/address-sort.ts
+++ packages/utils/src/address-sort.ts
@@ -21,12 +21,12 @@
  */
 
 import { isPrivate } from './multiaddr/is-private.js'
+import { Circuit } from '@multiformats/multiaddr-matcher'
 import type { Address } from '@libp2p/interface/peer-store'
 
 /**
- * Compare function for array.sort().
- * This sort aims to move the private addresses to the end of the array.
- * In case of equality, a certified address will come first.
+ * Compare function for array.sort() that moves public addresses to the start
+ * of the array.
  */
 export function publicAddressesFirst (a: Address, b: Address): -1 | 0 | 1 {
   const isAPrivate = isPrivate(a.multiaddr)
@@ -37,7 +37,15 @@ export function publicAddressesFirst (a: Address, b: Address): -1 | 0 | 1 {
   } else if (!isAPrivate && isBPrivate) {
     return -1
   }
-  // Check certified?
+
+  return 0
+}
+
+/**
+ * Compare function for array.sort() that moves certified addresses to the start
+ * of the array.
+ */
+export function certifiedAddressesFirst (a: Address, b: Address): -1 | 0 | 1 {
   if (a.isCertified && !b.isCertified) {
     return -1
   } else if (!a.isCertified && b.isCertified) {
@@ -48,8 +56,36 @@ export function publicAddressesFirst (a: Address, b: Address): -1 | 0 | 1 {
 }
 
 /**
- * A test thing
+ * Compare function for array.sort() that moves circuit relay addresses to the
+ * start of the array.
  */
-export async function something (): Promise<Uint8Array> {
-  return Uint8Array.from([0, 1, 2])
+export function circuitRelayAddressesLast (a: Address, b: Address): -1 | 0 | 1 {
+  const isACircuit = Circuit.exactMatch(a.multiaddr)
+  const isBCircuit = Circuit.exactMatch(b.multiaddr)
+
+  if (isACircuit && !isBCircuit) {
+    return 1
+  } else if (!isACircuit && isBCircuit) {
+    return -1
+  }
+
+  return 0
+}
+
+export function defaultAddressSort (a: Address, b: Address): -1 | 0 | 1 {
+  const publicResult = publicAddressesFirst(a, b)
+
+  if (publicResult !== 0) {
+    return publicResult
+  }
+
+  const relayResult = circuitRelayAddressesLast(a, b)
+
+  if (relayResult !== 0) {
+    return relayResult
+  }
+
+  const certifiedResult = certifiedAddressesFirst(a, b)
+
+  return certifiedResult
 }
