b975fbee9cce9995476344ff28982d2e299a8ee2
Alex Potsides <alex@achingbrain.net>
2023-11-09 14:05:24 +0000

    chore: add integration tests (#2224)
    Restores tests that need to spin up one or more libp2p nodes to complete.
    
    Eventually most `*.node.js` tests from the libp2p package should migrate here.

---
+++ packages/integration-tests/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @packageDocumentation
+ *
+ * The tests in this module ensure the various parts of libp2p work together as intended.
+ *
+ * They generally require starting one or more libp2p nodes so are here to prevent circular dependencies forming between modules.
+ */
+
+export {}
---
+++ packages/integration-tests/test/node.ts
@@ -0,0 +1,4 @@
+import './circuit-relay-discovery.node.js'
+import './circuit-relay.node.js'
+import './dcutr.node.js'
+import './identify.node.js'
