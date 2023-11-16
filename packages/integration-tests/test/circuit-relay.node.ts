41170986f1585db61a5475ff5bbe47156808b445
achingbrain <alex@achingbrain.net>
2023-11-16 08:49:50 +0000

    chore: undo edit

--- packages/integration-tests/test/circuit-relay.node.ts
+++ packages/integration-tests/test/circuit-relay.node.ts
@@ -535,9 +535,11 @@ describe('circuit-relay', () => {
       // relay1 dials relay2
       await relay1.dial(relay2.getMultiaddrs()[0])
       await usingAsRelay(relay1, relay2)
+
       // remote dials relay2
       await remote.dial(relay2.getMultiaddrs()[0])
       await usingAsRelay(remote, relay2)
+
       // local dials relay1 via relay2
       const ma = getRelayAddress(relay1)
 
@@ -545,7 +547,9 @@ describe('circuit-relay', () => {
       const stream = await local.dialProtocol(ma, RELAY_V2_HOP_CODEC, {
         runOnTransientConnection: true
       })
+
       const hopStream = pbStream(stream).pb(HopMessage)
+
       await hopStream.write({
         type: HopMessage.Type.CONNECT,
         peer: {
@@ -553,6 +557,7 @@ describe('circuit-relay', () => {
           addrs: []
         }
       })
+
       const response = await hopStream.read()
       expect(response).to.have.property('type', HopMessage.Type.STATUS)
       expect(response).to.have.property('status', Status.PERMISSION_DENIED)
