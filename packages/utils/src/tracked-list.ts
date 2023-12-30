84c7ad88b0e4a4ab37862502bde4f177c51754d6
achingbrain <alex@achingbrain.net>
2023-12-30 17:10:52 +0100

    feat: add tracked list to utils
    To allow tracking sizes of internal lists using libp2p metrics, add
    a tracked list type simmilar to tracked map.

---
+++ packages/utils/src/tracked-list.ts
@@ -0,0 +1,31 @@
+import type { Metrics } from '@libp2p/interface'
+
+export interface TrackedListInit {
+  name: string
+  metrics: Metrics
+}
+
+export interface CreateTrackedListInit {
+  /**
+   * The metric name to use
+   */
+  name: string
+
+  /**
+   * A metrics implementation
+   */
+  metrics?: Metrics
+}
+
+export function trackedList <V> (config: CreateTrackedListInit): V[] {
+  const { name, metrics } = config
+  const list: V[] = []
+
+  metrics?.registerMetric(name, {
+    calculate: () => {
+      return list.length
+    }
+  })
+
+  return list
+}
