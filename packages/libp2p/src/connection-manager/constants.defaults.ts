9740f4daff79ecd09185d3eb017fd9485f9228c1
achingbrain <alex@achingbrain.net>
2023-12-31 08:31:16 +0100

    chore: restore defaults

--- packages/libp2p/src/connection-manager/constants.defaults.ts
+++ packages/libp2p/src/connection-manager/constants.defaults.ts
@@ -1,17 +1,17 @@
 /**
  * @see https://libp2p.github.io/js-libp2p/interfaces/index._internal_.ConnectionManagerConfig.html#dialTimeout
  */
-export const DIAL_TIMEOUT = 5000
+export const DIAL_TIMEOUT = 30e3
 
 /**
  * @see https://libp2p.github.io/js-libp2p/interfaces/index._internal_.ConnectionManagerConfig.html#inboundUpgradeTimeout
  */
-export const INBOUND_UPGRADE_TIMEOUT = 5000
+export const INBOUND_UPGRADE_TIMEOUT = 30e3
 
 /**
  * @see https://libp2p.github.io/js-libp2p/interfaces/index._internal_.ConnectionManagerConfig.html#maxPeerAddrsToDial
  */
-export const MAX_PEER_ADDRS_TO_DIAL = 1
+export const MAX_PEER_ADDRS_TO_DIAL = 25
 
 /**
  * @see https://libp2p.github.io/js-libp2p/interfaces/index._internal_.ConnectionManagerConfig.html#autoDialInterval
