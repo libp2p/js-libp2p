cec00f6895ba49dc0164bd92e0d4fad64da0d03d
achingbrain <alex@achingbrain.net>
2024-01-24 11:24:16 +0100

    fix: allow typing job options in peer queue
    Allows specifying the job option type as a generic.

--- packages/utils/src/peer-queue.ts
+++ packages/utils/src/peer-queue.ts
@@ -4,19 +4,19 @@ import { Queue, type QueueAddOptions } from './queue/index.js'
 import type { Job } from './queue/job.js'
 import type { PeerId } from '@libp2p/interface'
 
-export interface PeerQueueOptions extends QueueAddOptions {
+export interface PeerQueueJobOptions extends QueueAddOptions {
   peerId: PeerId
 }
 
 /**
  * Extends Queue to add support for querying queued jobs by peer id
  */
-export class PeerQueue<JobReturnType = void> extends Queue<JobReturnType, PeerQueueOptions> {
+export class PeerQueue<JobReturnType = void, JobOptions extends PeerQueueJobOptions = PeerQueueJobOptions> extends Queue<JobReturnType, JobOptions> {
   has (peerId: PeerId): boolean {
     return this.find(peerId) != null
   }
 
-  find (peerId: PeerId): Job<PeerQueueOptions, JobReturnType> | undefined {
+  find (peerId: PeerId): Job<JobOptions, JobReturnType> | undefined {
     return this.queue.find(job => {
       return peerId.equals(job.options.peerId)
     })
