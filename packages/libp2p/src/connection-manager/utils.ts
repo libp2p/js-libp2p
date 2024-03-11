76750f53aa1525b6cf6edc36f8c3aa43cf497334
achingbrain <alex@achingbrain.net>
2024-03-11 17:53:41 +0100

    chore: check resolvers before resolving

--- packages/libp2p/src/connection-manager/utils.ts
+++ packages/libp2p/src/connection-manager/utils.ts
@@ -1,3 +1,4 @@
+import { resolvers } from '@multiformats/multiaddr'
 import type { LoggerOptions } from '@libp2p/interface'
 import type { DNS } from '@multiformats/dns'
 import type { AbortOptions, Multiaddr } from '@multiformats/multiaddr'
@@ -11,6 +12,22 @@ export interface ResolveOptions extends AbortOptions, LoggerOptions {
  * Resolve multiaddr recursively
  */
 export async function resolveMultiaddrs (ma: Multiaddr, options: ResolveOptions): Promise<Multiaddr[]> {
+  // check multiaddr resolvers
+  let resolvable = false
+
+  for (const key of resolvers.keys()) {
+    resolvable = ma.protoNames().includes(key)
+
+    if (resolvable) {
+      break
+    }
+  }
+
+  // return multiaddr if it is not resolvable
+  if (!resolvable) {
+    return [ma]
+  }
+
   const output = await ma.resolve(options)
 
   options.log('resolved %s to', ma, output.map(ma => ma.toString()))
