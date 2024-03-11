9ed741c1dd9041a7aef4095032153fc0b4edc20e
achingbrain <alex@achingbrain.net>
2024-03-11 15:28:56 +0100

    feat: support custom DNS resolvers
    Adds a `dns` config key that lets users specify an instance of the
    `DNS` interface from `@multiformats/dns`.
    
    This instance will be passed through to DNSADDR `multiaddr`s for use
    when the DNSLink TXT record is resolved.

--- packages/libp2p/src/connection-manager/utils.ts
+++ packages/libp2p/src/connection-manager/utils.ts
@@ -1,47 +1,19 @@
-import { type AbortOptions, multiaddr, type Multiaddr } from '@multiformats/multiaddr'
 import type { LoggerOptions } from '@libp2p/interface'
+import type { DNS } from '@multiformats/dns'
+import type { AbortOptions, Multiaddr } from '@multiformats/multiaddr'
+
+export interface ResolveOptions extends AbortOptions, LoggerOptions {
+  dns?: DNS
+  maxRecursiveDepth?: number
+}
 
 /**
  * Resolve multiaddr recursively
  */
-export async function resolveMultiaddrs (ma: Multiaddr, options: AbortOptions & LoggerOptions): Promise<Multiaddr[]> {
-  // TODO: recursive logic should live in multiaddr once dns4/dns6 support is in place
-  // Now only supporting resolve for dnsaddr
-  const resolvableProto = ma.protoNames().includes('dnsaddr')
-
-  // Multiaddr is not resolvable? End recursion!
-  if (!resolvableProto) {
-    return [ma]
-  }
-
-  const resolvedMultiaddrs = await resolveRecord(ma, options)
-  const recursiveMultiaddrs = await Promise.all(resolvedMultiaddrs.map(async (nm) => {
-    return resolveMultiaddrs(nm, options)
-  }))
-
-  const addrs = recursiveMultiaddrs.flat()
-  const output = addrs.reduce<Multiaddr[]>((array, newM) => {
-    if (array.find(m => m.equals(newM)) == null) {
-      array.push(newM)
-    }
-    return array
-  }, ([]))
+export async function resolveMultiaddrs (ma: Multiaddr, options: ResolveOptions): Promise<Multiaddr[]> {
+  const output = await ma.resolve(options)
 
   options.log('resolved %s to', ma, output.map(ma => ma.toString()))
 
   return output
-}
-
-/**
- * Resolve a given multiaddr. If this fails, an empty array will be returned
- */
-async function resolveRecord (ma: Multiaddr, options: AbortOptions & LoggerOptions): Promise<Multiaddr[]> {
-  try {
-    ma = multiaddr(ma.toString()) // Use current multiaddr module
-    const multiaddrs = await ma.resolve(options)
-    return multiaddrs
-  } catch (err) {
-    options.log.error(`multiaddr ${ma.toString()} could not be resolved`, err)
-    return []
-  }
 }
