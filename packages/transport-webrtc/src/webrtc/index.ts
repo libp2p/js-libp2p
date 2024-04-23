3ec4de0b2133e05a944f71df5ba6e99b2903bb20
achingbrain <alex@achingbrain.net>
2024-04-23 11:11:05 +0100

    fix: do not run webrtc cleanup on transport close
    The cleanup function from `node-datachannel` closes all open peer
    connections/datachannels - if there are multiple transports/nodes
    in the same process they will all be closed.

--- packages/transport-webrtc/src/webrtc/index.ts
+++ packages/transport-webrtc/src/webrtc/index.ts
@@ -1,7 +1,1 @@
-import node from 'node-datachannel'
-
 export { RTCSessionDescription, RTCIceCandidate, RTCPeerConnection } from 'node-datachannel/polyfill'
-
-export function cleanup (): void {
-  node.cleanup()
-}
