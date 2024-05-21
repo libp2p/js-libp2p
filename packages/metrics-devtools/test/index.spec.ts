3c2ed107ebd0d73d6717fc1d68027e788c2e0630
achingbrain <alex@achingbrain.net>
2024-05-21 12:45:20 +0100

    chore: switch back to ports

--- packages/metrics-devtools/test/index.spec.ts
+++ packages/metrics-devtools/test/index.spec.ts
@@ -2,8 +2,9 @@ import { TypedEventEmitter, start, stop } from '@libp2p/interface'
 import { defaultLogger } from '@libp2p/logger'
 import { createEd25519PeerId } from '@libp2p/peer-id-factory'
 import { expect } from 'aegir/chai'
+import { raceEvent } from 'race-event'
 import { stubInterface, type StubbedInstance } from 'sinon-ts'
-import { LIBP2P_DEVTOOLS_METRICS_INSTANCE, devToolsMetrics } from '../src/index.js'
+import { LIBP2P_DEVTOOLS_METRICS_KEY, SOURCE_METRICS, SOURCE_DEVTOOLS, devToolsMetrics, type ApplicationMessage } from '../src/index.js'
 import type { ComponentLogger, Libp2pEvents, Metrics, PeerId, PeerStore } from '@libp2p/interface'
 import type { ConnectionManager, Registrar, TransportManager } from '@libp2p/interface-internal'
 
@@ -43,24 +44,43 @@ describe('devtools-metrics', () => {
     await stop(metrics)
   })
 
-  it('should signal presence of metrics', () => {
-    expect(globalThis).to.have.property(LIBP2P_DEVTOOLS_METRICS_INSTANCE).that.is.ok()
-  })
-
   it('should broadcast metrics', async () => {
-    // @ts-expect-error type is ambiguous
-    const metrics = globalThis[LIBP2P_DEVTOOLS_METRICS_INSTANCE].getMetrics()
+    const event = await raceEvent<MessageEvent<ApplicationMessage>>(window, 'message', AbortSignal.timeout(1000), {
+      filter: (evt) => {
+        return evt.data.source === SOURCE_METRICS && evt.data.type === 'metrics'
+      }
+    })
 
-    expect(metrics).to.be.ok()
+    expect(event).to.have.nested.property('data.metrics')
   })
 
   it('should identify node', async () => {
     components.transportManager.getListeners.returns([])
     components.registrar.getProtocols.returns([])
+    components.peerStore.get.withArgs(components.peerId).resolves({
+      id: components.peerId,
+      addresses: [],
+      metadata: new Map(),
+      protocols: [],
+      tags: new Map()
+    })
 
-    // @ts-expect-error type is ambiguous
-    const status = globalThis[LIBP2P_DEVTOOLS_METRICS_INSTANCE].getStatus()
+    // devtools asks the node to reveal itself
+    window.postMessage({
+      source: SOURCE_DEVTOOLS,
+      type: 'identify'
+    }, '*')
 
-    expect(status).to.have.property('peerId')
+    const event = await raceEvent<MessageEvent<ApplicationMessage>>(window, 'message', AbortSignal.timeout(1000), {
+      filter: (evt) => {
+        return evt.data.source === SOURCE_METRICS && evt.data.type === 'self'
+      }
+    })
+
+    expect(event).to.have.nested.property('data.peer.id')
+  })
+
+  it('should signal presence of metrics', () => {
+    expect(globalThis).to.have.property(LIBP2P_DEVTOOLS_METRICS_KEY).that.is.true()
   })
 })
