3c8dd5bbfc57489a0b10b555c81e773058a58156
Joel Gustafson <joelg@mit.edu>
2024-08-16 13:36:14 -0400

    fix: accept custom ping protocol prefix in connection monitor (#2667)
    The new connection monitor currently uses the hard-coded default ping protocol `'/ipfs/ping/1.0.0'`. The ping service lets the developer override the `ipfs` component with a custom protocol prefix, and some apps are already doing this. This PR adds support for a `protocolPrefix` option in `ConnectionMonitorInit` that matches the option in `PingServiceInit`.
    
    ---------
    
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

--- packages/libp2p/src/connection-monitor.ts
+++ packages/libp2p/src/connection-monitor.ts
@@ -6,6 +6,9 @@ import type { ConnectionManager } from '@libp2p/interface-internal'
 import type { AdaptiveTimeoutInit } from '@libp2p/utils/adaptive-timeout'
 
 const DEFAULT_PING_INTERVAL_MS = 10000
+const PROTOCOL_VERSION = '1.0.0'
+const PROTOCOL_NAME = 'ping'
+const PROTOCOL_PREFIX = 'ipfs'
 
 export interface ConnectionMonitorInit {
   /**
@@ -37,6 +40,13 @@ export interface ConnectionMonitorInit {
    * @default true
    */
   abortConnectionOnPingFailure?: boolean
+
+  /**
+   * Override the ping protocol prefix
+   *
+   * @default 'ipfs'
+   */
+  protocolPrefix?: string
 }
 
 export interface ConnectionMonitorComponents {
@@ -46,6 +56,7 @@ export interface ConnectionMonitorComponents {
 }
 
 export class ConnectionMonitor implements Startable {
+  private readonly protocol: string
   private readonly components: ConnectionMonitorComponents
   private readonly log: Logger
   private heartbeatInterval?: ReturnType<typeof setInterval>
@@ -55,6 +66,7 @@ export class ConnectionMonitor implements Startable {
 
   constructor (components: ConnectionMonitorComponents, init: ConnectionMonitorInit = {}) {
     this.components = components
+    this.protocol = `/${init.protocolPrefix ?? PROTOCOL_PREFIX}/${PROTOCOL_NAME}/${PROTOCOL_VERSION}`
 
     this.log = components.logger.forComponent('libp2p:connection-monitor')
     this.pingIntervalMs = init.pingInterval ?? DEFAULT_PING_INTERVAL_MS
@@ -83,7 +95,7 @@ export class ConnectionMonitor implements Startable {
             const signal = this.timeout.getTimeoutSignal({
               signal: this.abortController?.signal
             })
-            const stream = await conn.newStream('/ipfs/ping/1.0.0', {
+            const stream = await conn.newStream(this.protocol, {
               signal,
               runOnTransientConnection: true
             })
