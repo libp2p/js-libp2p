1b001dfabf3dacc1bb8a758cc908bd6fd3c72a42
achingbrain <alex@achingbrain.net>
2024-09-20 10:51:33 +0200

    chore: test on firefox

--- packages/metrics-devtools/src/utils/gather-capabilities.ts
+++ packages/metrics-devtools/src/utils/gather-capabilities.ts
@@ -2,7 +2,7 @@ import { serviceCapabilities } from '@libp2p/interface'
 
 export function gatherCapabilities (components: any): Record<string, string[]> {
   const capabilities: Record<string, string[]> = {}
-  const services: Record<string, any> = components.components
+  const services: Record<string, any> = components.components ?? components
 
   Object.entries(services).forEach(([name, component]) => {
     if (component?.[serviceCapabilities] != null && Array.isArray(component[serviceCapabilities])) {
