0b72656435f789ffe2b6ba2b0dc2c7fe71feb96b
achingbrain <alex@achingbrain.net>
2024-11-04 22:29:08 +0000

    feat: add memory transport
    Adds an in-memory only transport using the `/memory/...` multiaddr
    protocol.
    
    This lets us connect multiple in-process nodes together with
    controlable latency settings to aid testing.

---
+++ packages/transport-memory/src/index.ts
@@ -0,0 +1,62 @@
+/**
+ * @packageDocumentation
+ *
+ * A [libp2p transport](https://docs.libp2p.io/concepts/transports/overview/)
+ * that operates in-memory only.
+ *
+ * This is intended for testing and can only be used to connect two libp2p nodes
+ * that are running in the same process.
+ *
+ * @example
+ *
+ * ```TypeScript
+ * import { createLibp2p } from 'libp2p'
+ * import { memory } from '@libp2p/memory'
+ * import { multiaddr } from '@multiformats/multiaddr'
+ *
+ * const listener = await createLibp2p({
+ *   addresses: {
+ *     listen: [
+ *       '/memory/address-a'
+ *     ]
+ *   },
+ *   transports: [
+ *     memory()
+ *   ]
+ * })
+ *
+ * const dialer = await createLibp2p({
+ *   transports: [
+ *     memory()
+ *   ]
+ * })
+ *
+ * const ma = multiaddr('/memory/address-a')
+ *
+ * // dial the listener, timing out after 10s
+ * const connection = await dialer.dial(ma, {
+ *   signal: AbortSignal.timeout(10_000)
+ * })
+ *
+ * // use connection...
+ * ```
+ */
+
+import { MemoryTransport } from './memory.js'
+import type { Transport, ComponentLogger, UpgraderOptions, PeerId } from '@libp2p/interface'
+
+export interface MemoryTransportComponents {
+  peerId: PeerId
+  logger: ComponentLogger
+}
+
+export interface MemoryTransportInit {
+  upgraderOptions?: UpgraderOptions
+  inboundUpgradeTimeout?: number
+}
+
+export function memory (init?: MemoryTransportInit): (components: MemoryTransportComponents) => Transport {
+  return (components) => {
+    return new MemoryTransport(components, init)
+  }
+}
