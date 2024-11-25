2046823aece110929261ef22408f2187ae0d0a49
Alex Potsides <alex@achingbrain.net>
2024-11-25 09:01:44 +0000

    chore: revert merge

--- packages/transport-websockets/test/browser.ts
+++ packages/transport-websockets/test/browser.ts
@@ -6,33 +6,6 @@ import { expect } from 'aegir/chai'
 import { webSockets } from '../src/index.js'
 
 describe('libp2p-websockets', () => {
-  let ws: Transport
-
-  beforeEach(async () => {
-    const events = new TypedEventEmitter()
-
-    ws = webSockets()({
-      events,
-      logger: defaultLogger()
-    })
-  })
-
-  it('should filter out no wss websocket addresses', function () {
-    const ma1 = multiaddr('/ip4/127.0.0.1/tcp/80/ws')
-    const ma2 = multiaddr('/ip4/127.0.0.1/tcp/443/wss')
-    const ma3 = multiaddr('/ip6/::1/tcp/80/ws')
-    const ma4 = multiaddr('/ip6/::1/tcp/443/wss')
-
-    const valid = ws.dialFilter([ma1, ma2, ma3, ma4])
-
-    if (isBrowser || isWebWorker) {
-      expect(valid.length).to.equal(2)
-      expect(valid).to.deep.equal([ma2, ma4])
-    } else {
-      expect(valid.length).to.equal(4)
-    }
-  })
-
   it('.createServer throws in browser', () => {
     expect(webSockets()({
       events: new TypedEventEmitter(),
