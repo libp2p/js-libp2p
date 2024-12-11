cee3c519e18232f5e39ed5f8c5679274989d3459
achingbrain <alex@achingbrain.net>
2024-12-11 18:43:18 +0000

    fix: increase max autonat streams and limit incoming message size
    Allows more outgoing streams to be open at the same time and limits
    the allowed incoming message size.

--- packages/protocol-autonat/src/constants.ts
+++ packages/protocol-autonat/src/constants.ts
@@ -13,6 +13,7 @@ export const PROTOCOL_NAME = 'autonat'
  */
 export const PROTOCOL_VERSION = '1.0.0'
 export const TIMEOUT = 30000
-export const MAX_INBOUND_STREAMS = 1
-export const MAX_OUTBOUND_STREAMS = 1
+export const MAX_INBOUND_STREAMS = 2
+export const MAX_OUTBOUND_STREAMS = 20
 export const DEFAULT_CONNECTION_THRESHOLD = 80
+export const MAX_MESSAGE_SIZE = 8192
