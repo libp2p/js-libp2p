dec817830ef20e80ae5f9e8985074013fb4bb0b3
achingbrain <alex@achingbrain.net>
2024-12-19 18:31:12 +0000

    feat: add OpenTelemetry metrics implementation
    Adds a `Metrics` interface implementation that is powered by an
    OpenTelemetry backend.
    
    See the readme for a usage example.

---
+++ packages/metrics-opentelemetry/test/index.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'aegir/chai'
+
+describe('opentelemetry-metrics', () => {
+  it('should wrap a method', async () => {
+    // TODO: tests
+    expect(true).to.be.true()
+  })
+})
