b56d918848b22febde02171e90d2d04eb07203a4
Alex Potsides <alex@achingbrain.net>
2025-01-09 11:18:07 +0100

    fix!: accept Uint8Arrays as keys (#2909)
    To allow use cases like fetching IPNS records in a way compatible with go-libp2p we need to send binary as fetch identifiers.
    
    JavaScript strings are UTF-16 so we can't round-trip binary reliably since some byte sequences are interpreted as multi-byte or otherwise non-printable characters.
    
    Instead we need to accept Uint8Arrays and send them over the wire as-is.
    
    This is a backwards compatible change as far as interop goes since protobuf `bytes` and `string` types are identical on the wire, but it's breaking for API consumers in that the lookup function now needs to accept a `Uint8Array` identifier instead of a `string`.
    
    Refs: https://github.com/libp2p/specs/issues/656
    
    BREAKING CHANGE: registered lookup functions now receive a Uint8Array identifier instead of a string

--- packages/protocol-fetch/src/pb/proto.ts
+++ packages/protocol-fetch/src/pb/proto.ts
@@ -9,7 +9,7 @@ import { alloc as uint8ArrayAlloc } from 'uint8arrays/alloc'
 import type { Uint8ArrayList } from 'uint8arraylist'
 
 export interface FetchRequest {
-  identifier: string
+  identifier: Uint8Array
 }
 
 export namespace FetchRequest {
@@ -22,9 +22,9 @@ export namespace FetchRequest {
           w.fork()
         }
 
-        if ((obj.identifier != null && obj.identifier !== '')) {
+        if ((obj.identifier != null && obj.identifier.byteLength > 0)) {
           w.uint32(10)
-          w.string(obj.identifier)
+          w.bytes(obj.identifier)
         }
 
         if (opts.lengthDelimited !== false) {
@@ -32,7 +32,7 @@ export namespace FetchRequest {
         }
       }, (reader, length, opts = {}) => {
         const obj: any = {
-          identifier: ''
+          identifier: uint8ArrayAlloc(0)
         }
 
         const end = length == null ? reader.len : reader.pos + length
@@ -42,7 +42,7 @@ export namespace FetchRequest {
 
           switch (tag >>> 3) {
             case 1: {
-              obj.identifier = reader.string()
+              obj.identifier = reader.bytes()
               break
             }
             default: {
