d68bfc06ebf42bd1d76b2c637aae2594535899d3
achingbrain <alex@achingbrain.net>
2025-01-27 15:23:36 +0100

    chore: legibility

--- packages/utils/src/is-generator.ts
+++ packages/utils/src/is-generator.ts
@@ -1,8 +1,15 @@
 export function isGenerator (obj: unknown): obj is Generator {
-  if (obj == null) return false
+  if (obj == null) {
+    return false
+  }
+
   const iterator = (obj as { [Symbol.iterator]?: unknown })?.[Symbol.iterator]
-  if (typeof iterator !== 'function') return false
+
+  if (typeof iterator !== 'function') {
+    return false
+  }
 
   const instance = obj as { next?: unknown }
+
   return typeof instance.next === 'function'
 }
