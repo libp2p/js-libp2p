a32fbeb1dbf1ffbb59445f56ba011af4123ab085
Alex Potsides <alex@achingbrain.net>
2025-02-03 12:05:30 +0100

    fix: use isPromise/etc function from utils (#2925)
    Includes improvements to detection from #2918
    
    ---------
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    Co-authored-by: Sacha Froment <sfroment42@gmail.com>

--- packages/utils/src/is-async-generator.ts
+++ packages/utils/src/is-async-generator.ts
@@ -1,9 +1,15 @@
 export function isAsyncGenerator (obj: unknown): obj is AsyncGenerator {
-  if (obj == null) return false
+  if (obj == null) {
+    return false
+  }
+
   const asyncIterator = (obj as { [Symbol.asyncIterator]?: unknown })?.[
     Symbol.asyncIterator
   ]
-  if (typeof asyncIterator !== 'function') return false
+
+  if (typeof asyncIterator !== 'function') {
+    return false
+  }
 
   const instance = obj as { next?: unknown }
   return typeof instance.next === 'function'
