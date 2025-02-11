f0325d372e860e490d312878cfe260a862f49d16
achingbrain <alex@achingbrain.net>
2025-02-11 21:29:35 +0100

    chore: remove logging

--- packages/transport-webrtc/src/webrtc/index.ts
+++ packages/transport-webrtc/src/webrtc/index.ts
@@ -1,6 +1,2 @@
-import { initLogger } from '@ipshipyard/node-datachannel'
-
-initLogger('Verbose')
-
 export { RTCSessionDescription, RTCIceCandidate, RTCPeerConnection } from '@ipshipyard/node-datachannel/polyfill'
 export type { RTCDataChannel, RTCDataChannelEvent } from '@ipshipyard/node-datachannel/polyfill'
