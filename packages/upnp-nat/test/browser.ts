d188511b97ef9fc320233d284d16a6275b029b81
Alex Potsides <alex@achingbrain.net>
2025-02-13 10:37:07 +0100

    fix: add browser polyfill to upnp-nat (#2959)
    To give more helpful error messages, add a browser shim for upnp-nat, similar to `@libp2p/tcp` - without this the user gets errors about not being able to load node api modules, instead throw a specific error that tells them their configuration is invalid.

---
+++ packages/upnp-nat/test/browser.ts
@@ -0,0 +1,29 @@
+import { generateKeyPair } from '@libp2p/crypto/keys'
+import { TypedEventEmitter } from '@libp2p/interface'
+import { defaultLogger } from '@libp2p/logger'
+import { peerIdFromPrivateKey } from '@libp2p/peer-id'
+import { expect } from 'aegir/chai'
+import { stubInterface } from 'sinon-ts'
+import { isBrowser, isWebWorker } from 'wherearewe'
+import { uPnPNAT } from '../src/index.js'
+import type { AddressManager } from '@libp2p/interface-internal'
+
+describe('browser non-support', () => {
+  it('should throw in browsers', async function () {
+    if (!isBrowser && !isWebWorker) {
+      return this.skip()
+    }
+
+    const components = {
+      peerId: peerIdFromPrivateKey(await generateKeyPair('Ed25519')),
+      nodeInfo: { name: 'test', version: 'test', userAgent: 'test' },
+      logger: defaultLogger(),
+      addressManager: stubInterface<AddressManager>(),
+      events: new TypedEventEmitter()
+    }
+
+    expect(() => {
+      uPnPNAT()(components)
+    }).to.throw()
+  })
+})
