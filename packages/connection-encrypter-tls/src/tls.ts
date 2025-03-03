f5ef31dd07df7bab500b9eefb939ced035af93b1
achingbrain <alex@achingbrain.net>
2025-03-03 17:41:31 +0100

    chore: send libp2p alpn

--- packages/connection-encrypter-tls/src/tls.ts
+++ packages/connection-encrypter-tls/src/tls.ts
@@ -19,7 +19,7 @@
  */
 
 import { TLSSocket, type TLSSocketOptions, connect } from 'node:tls'
-import { serviceCapabilities } from '@libp2p/interface'
+import { InvalidCryptoExchangeError, serviceCapabilities } from '@libp2p/interface'
 import { HandshakeTimeoutError } from './errors.js'
 import { generateCertificate, verifyPeerCertificate, itToStream, streamToIt } from './utils.js'
 import { PROTOCOL } from './index.js'
@@ -100,10 +100,11 @@ export class TLS implements ConnectionEncrypter {
 
       // early negotiation of muxer via ALPN protocols
       ALPNProtocols: [
-        ...this.components.upgrader.getStreamMuxers().keys()
+        ...this.components.upgrader.getStreamMuxers().keys(),
+        'libp2p'
       ],
       ALPNCallback: ({ servername, protocols }) => {
-        this.log('received server name %s and protocols %s', servername, protocols)
+        this.log.trace('received server name %s and protocols %s', servername, protocols)
 
         for (const protocol of protocols) {
           streamMuxer = this.components.upgrader.getStreamMuxers().get(protocol)
@@ -153,6 +154,23 @@ export class TLS implements ConnectionEncrypter {
 
               if (streamMuxer == null) {
                 this.log.error('selected muxer that did not exist')
+              }
+            }
+
+            // 'libp2p' is a special protocol - if it's sent the remote does not
+            // support early muxer negotiation
+            if (!isServer && typeof socket.alpnProtocol === 'string' && socket.alpnProtocol !== 'libp2p') {
+              this.log.trace('got early muxer', socket.alpnProtocol)
+              streamMuxer = this.components.upgrader.getStreamMuxers().get(socket.alpnProtocol)
+
+              if (streamMuxer == null) {
+                const err = new InvalidCryptoExchangeError(`Selected muxer ${socket.alpnProtocol} did not exist`)
+                this.log.error(`Selected muxer ${socket.alpnProtocol} did not exist - %e`, err)
+
+                if (isAbortable(conn)) {
+                  conn.abort(err)
+                  reject(err)
+                }
               }
             }
 
