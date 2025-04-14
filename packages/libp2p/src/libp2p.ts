f3e2cd974d50087d3ec2484f006cff7e51cd12e1
dozyio <github@dozy.io>
2025-04-14 17:25:06 +0100

    chore: spelling

--- packages/libp2p/src/libp2p.ts
+++ packages/libp2p/src/libp2p.ts
@@ -132,7 +132,7 @@ export class Libp2p<T extends ServiceMap = ServiceMap> extends TypedEventEmitter
       this.configureComponent('connectionMonitor', new ConnectionMonitor(this.components, init.connectionMonitor))
     }
 
-    // Set up a custom registar if configured or use default
+    // Set up a custom registrar if configured or use default
     if (init.registrar != null) {
       this.configureComponent('registrar', init.registrar(this.components))
     } else {
