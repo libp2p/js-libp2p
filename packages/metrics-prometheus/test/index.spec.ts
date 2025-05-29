01328a0b4eab0a66d5805d9ad4b6f25dbbdb4b03
Alex Potsides <alex@achingbrain.net>
2025-05-29 07:33:34 +0100

    fix: clear references to metrics on stop (#3154)
    Otherwise the node can leak memory.

---
+++ packages/metrics-prometheus/test/index.spec.ts
@@ -0,0 +1,36 @@
+import { start, stop } from '@libp2p/interface'
+import { defaultLogger } from '@libp2p/logger'
+import { expect } from 'aegir/chai'
+import { prometheusMetrics } from '../src/index.js'
+import type { Metrics } from '@libp2p/interface'
+
+describe('simple-metrics', () => {
+  let metrics: Metrics
+
+  afterEach(async () => {
+    if (metrics != null) {
+      await stop(metrics)
+    }
+  })
+
+  it('should not retain metrics after stop', async () => {
+    metrics = prometheusMetrics()({
+      logger: defaultLogger()
+    })
+
+    await start(metrics)
+
+    const m1 = metrics.registerCounterGroup('test_metric')
+    const m2 = metrics.registerCounterGroup('test_metric')
+
+    expect(m1).to.equal(m2, 'did not re-use metric')
+
+    await stop(metrics)
+
+    await start(metrics)
+
+    const m3 = metrics.registerCounterGroup('test_metric')
+
+    expect(m3).to.not.equal(m1, 're-used metric')
+  })
+})
