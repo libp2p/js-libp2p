8e87be9e6854b4508b2682535c7c9a4013bb5537
Chandelier-02 <79105946+Chandelier-02@users.noreply.github.com>
2025-06-03 07:50:37 -0500

    fix: detect Ed25519 support in WebCrypto (#3100)
    Try creating an Ed25519 key, if it's successful, use WebCrypto for all Ed25519 operations instead of the pure-js `@noble/curves` module for the performance benefit.
    
    ---------
    
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

--- packages/crypto/src/keys/ed25519/ed25519.ts
+++ packages/crypto/src/keys/ed25519/ed25519.ts
@@ -2,6 +2,7 @@ import { base58btc } from 'multiformats/bases/base58'
 import { CID } from 'multiformats/cid'
 import { identity } from 'multiformats/hashes/identity'
 import { equals as uint8ArrayEquals } from 'uint8arrays/equals'
+import { isPromise } from '../../util.ts'
 import { publicKeyToProtobuf } from '../index.js'
 import { ensureEd25519Key } from './utils.js'
 import * as crypto from './index.js'
@@ -37,9 +38,18 @@ export class Ed25519PublicKey implements Ed25519PublicKeyInterface {
     return uint8ArrayEquals(this.raw, key.raw)
   }
 
-  verify (data: Uint8Array | Uint8ArrayList, sig: Uint8Array, options?: AbortOptions): boolean {
+  verify (data: Uint8Array | Uint8ArrayList, sig: Uint8Array, options?: AbortOptions): boolean | Promise<boolean> {
     options?.signal?.throwIfAborted()
-    return crypto.hashAndVerify(this.raw, sig, data)
+    const result = crypto.hashAndVerify(this.raw, sig, data)
+
+    if (isPromise<boolean>(result)) {
+      return result.then(res => {
+        options?.signal?.throwIfAborted()
+        return res
+      })
+    }
+
+    return result
   }
 }
 
@@ -63,8 +73,18 @@ export class Ed25519PrivateKey implements Ed25519PrivateKeyInterface {
     return uint8ArrayEquals(this.raw, key.raw)
   }
 
-  sign (message: Uint8Array | Uint8ArrayList, options?: AbortOptions): Uint8Array {
+  sign (message: Uint8Array | Uint8ArrayList, options?: AbortOptions): Uint8Array | Promise<Uint8Array> {
     options?.signal?.throwIfAborted()
-    return crypto.hashAndSign(this.raw, message)
+    const sig = crypto.hashAndSign(this.raw, message)
+
+    if (isPromise<Uint8Array>(sig)) {
+      return sig.then(res => {
+        options?.signal?.throwIfAborted()
+        return res
+      })
+    }
+
+    options?.signal?.throwIfAborted()
+    return sig
   }
 }
--- packages/crypto/test/keys/ed25519.spec.ts
+++ packages/crypto/test/keys/ed25519.spec.ts
@@ -57,7 +57,7 @@ describe('ed25519', function () {
   it('signs', async () => {
     const text = randomBytes(512)
     const sig = await key.sign(text)
-    const res = key.publicKey.verify(text, sig)
+    const res = await key.publicKey.verify(text, sig)
     expect(res).to.be.be.true()
   })
 
@@ -68,12 +68,12 @@ describe('ed25519', function () {
     )
     const sig = await key.sign(text)
 
-    expect(key.sign(text.subarray()))
+    expect(await key.sign(text.subarray()))
       .to.deep.equal(sig, 'list did not have same signature as a single buffer')
 
-    expect(key.publicKey.verify(text, sig))
+    expect(await key.publicKey.verify(text, sig))
       .to.be.true('did not verify message as list')
-    expect(key.publicKey.verify(text.subarray(), sig))
+    expect(await key.publicKey.verify(text.subarray(), sig))
       .to.be.true('did not verify message as single buffer')
   })
 
@@ -143,7 +143,7 @@ describe('ed25519', function () {
   it('sign and verify', async () => {
     const data = uint8ArrayFromString('hello world')
     const sig = await key.sign(data)
-    const valid = key.publicKey.verify(data, sig)
+    const valid = await key.publicKey.verify(data, sig)
     expect(valid).to.be.true()
   })
 
@@ -159,7 +159,7 @@ describe('ed25519', function () {
   it('fails to verify for different data', async () => {
     const data = uint8ArrayFromString('hello world')
     const sig = await key.sign(data)
-    const valid = key.publicKey.verify(uint8ArrayFromString('hello'), sig)
+    const valid = await key.publicKey.verify(uint8ArrayFromString('hello'), sig)
     expect(valid).to.be.be.false()
   })
 
