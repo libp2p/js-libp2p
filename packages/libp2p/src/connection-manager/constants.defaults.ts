1c1c49ef4f25dcd8925d134f7e185658c10d2d6b
Alex Potsides <alex@achingbrain.net>
2025-06-25 15:42:48 +0200

    fix: add multiaddr resolvers (#3200)
    Use internal multiaddr resolvers so they can be removed from the
    Multiaddr class to make it more lightweight.

--- packages/libp2p/src/connection-manager/constants.defaults.ts
+++ packages/libp2p/src/connection-manager/constants.defaults.ts
@@ -54,3 +54,8 @@ export const LAST_DIAL_SUCCESS_KEY = 'last-dial-success'
  * @see https://libp2p.github.io/js-libp2p/interfaces/index._internal_.ConnectionManagerConfig.html#maxDialQueueLength
  */
 export const MAX_DIAL_QUEUE_LENGTH = 500
+
+/**
+ * @see https://libp2p.github.io/js-libp2p/interfaces/index._internal_.ConnectionManagerConfig.html#maxRecursiveDepth
+ */
+export const MAX_RECURSIVE_DEPTH = 32
