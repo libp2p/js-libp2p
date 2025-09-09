82fdf3e9fd085184303ae863763d9cecfc05c3d8
achingbrain <alex@achingbrain.net>
2025-09-09 16:45:19 +0300

    chore: let initiator close channel

--- packages/transport-webrtc/src/muxer.ts
+++ packages/transport-webrtc/src/muxer.ts
@@ -90,7 +90,7 @@ export class DataChannelMuxer extends AbstractStreamMuxer<WebRTCStream> implemen
      * {@link https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/datachannel_event}
      */
     this.peerConnection.ondatachannel = ({ channel }) => {
-      this.log.trace('incoming %s datachannel with channel id %d and status', channel.protocol, channel.id, channel.readyState)
+      this.log.trace('incoming %s datachannel with channel id %d, protocol %s and status %s', channel.protocol, channel.id, channel.protocol, channel.readyState)
 
       // 'init' channel is only used during connection establishment, it is
       // closed by the initiator
@@ -117,7 +117,7 @@ export class DataChannelMuxer extends AbstractStreamMuxer<WebRTCStream> implemen
     // The spec says the label MUST be an empty string: https://github.com/libp2p/specs/blob/master/webrtc/README.md#rtcdatachannel-label
     const channel = this.peerConnection.createDataChannel('', {
       // TODO: pre-negotiate stream protocol
-      // protocol: options?.protocol
+      protocol: options?.protocol
     })
 
     this.log('open channel %d for protocol %s', channel.id, options?.protocol)
