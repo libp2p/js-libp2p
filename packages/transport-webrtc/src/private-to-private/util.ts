05ce97e079f3b6d0196745e26ed3109b8b393df6
achingbrain <alex@achingbrain.net>
2025-09-09 18:01:17 +0300

    chore: try remove ff detection

--- packages/transport-webrtc/src/private-to-private/util.ts
+++ packages/transport-webrtc/src/private-to-private/util.ts
@@ -82,8 +82,10 @@ export function getConnectionState (pc: RTCPeerConnection): string {
 }
 
 function resolveOnConnected (pc: RTCPeerConnection, promise: DeferredPromise<void>): void {
-  pc[isFirefox ? 'oniceconnectionstatechange' : 'onconnectionstatechange'] = (_) => {
-    switch (getConnectionState(pc)) {
+  //pc[isFirefox ? 'oniceconnectionstatechange' : 'onconnectionstatechange'] = (_) => {
+  pc.onconnectionstatechange = (_) => {
+    //switch (getConnectionState(pc)) {
+    switch (pc.connectionState) {
       case 'connected':
         promise.resolve()
         break
