fe3547d3ee572de6c93045a4218a7c9d29d98702
achingbrain <alex@achingbrain.net>
2025-09-20 07:11:52 +0100

    chore: add comment

--- packages/utils/src/mock-muxer.ts
+++ packages/utils/src/mock-muxer.ts
@@ -279,6 +279,8 @@ class MockMuxer extends AbstractStreamMuxer<MockMuxedStream> {
       sendMessage: this.sendMessage,
       encoding: this.encoding,
       maxMessageSize: this.maxMessageSize - MESSAGE_OVERHEAD,
+      // TODO: uncomment this after removing gossipsub from the codebase (post
+      // v3 release), then add multistream-select to gossipsub tests
       // protocol: ''
     })
   }
