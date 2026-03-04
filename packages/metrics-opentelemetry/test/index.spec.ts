7c6a553ac3399576512ad05df57be3b0768034ab
lodekeeper <lodekeeper@users.noreply.github.com>
2026-03-04 07:31:42 +0000

    test(metrics): add parity stream tests for simple and opentelemetry

--- packages/metrics-opentelemetry/test/index.spec.ts
+++ packages/metrics-opentelemetry/test/index.spec.ts
@@ -1,6 +1,8 @@
 import { start, stop } from '@libp2p/interface'
 import { defaultLogger } from '@libp2p/logger'
+import { multiaddrConnectionPair, streamPair } from '@libp2p/utils'
 import { expect } from 'aegir/chai'
+import { pEvent } from 'p-event'
 import { openTelemetryMetrics } from '../src/index.js'
 
 describe('opentelemetry-metrics', () => {
@@ -25,6 +27,80 @@ describe('opentelemetry-metrics', () => {
     })
 
     expect(wrapped).to.not.equal(target.wrapped)
+  })
+
+  it('should track bytes over protocol streams without consuming early frames', async () => {
+    const metrics = openTelemetryMetrics()({
+      nodeInfo: {
+        name: 'test',
+        version: '1.0.0',
+        userAgent: 'test/1.0.0 node/1.0.0'
+      },
+      logger: defaultLogger()
+    })
+
+    const [outbound, inbound] = await streamPair({
+      protocol: '/echo/1.0.0'
+    })
+
+    metrics.trackProtocolStream(outbound)
+
+    const data = Uint8Array.from([7, 6, 5, 4, 3])
+    inbound.send(data)
+
+    const iterator = outbound[Symbol.asyncIterator]()
+    const first = await Promise.race([
+      iterator.next(),
+      new Promise<never>((_resolve, reject) => setTimeout(() => reject(new Error('timed out waiting for first frame')), 200))
+    ])
+
+    expect(first.done).to.equal(false)
+    expect(first.value?.byteLength).to.equal(data.length)
+
+    await Promise.all([
+      pEvent(inbound, 'close'),
+      outbound.close(),
+      inbound.close()
+    ])
+
+    const transferStats = (metrics as any).transferStats as Map<string, number>
+    expect(transferStats.get('/echo/1.0.0 received')).to.equal(data.length)
+  })
+
+  it('should track bytes over multiaddr connections', async () => {
+    const metrics = openTelemetryMetrics()({
+      nodeInfo: {
+        name: 'test',
+        version: '1.0.0',
+        userAgent: 'test/1.0.0 node/1.0.0'
+      },
+      logger: defaultLogger()
+    })
+
+    const [outbound, inbound] = multiaddrConnectionPair()
+
+    metrics.trackMultiaddrConnection(outbound)
+
+    const data = Uint8Array.from([0, 1, 2, 3, 4])
+    inbound.send(data)
+
+    const iterator = outbound[Symbol.asyncIterator]()
+    const first = await Promise.race([
+      iterator.next(),
+      new Promise<never>((_resolve, reject) => setTimeout(() => reject(new Error('timed out waiting for first frame')), 200))
+    ])
+
+    expect(first.done).to.equal(false)
+    expect(first.value?.byteLength).to.equal(data.length)
+
+    await Promise.all([
+      pEvent(inbound, 'close'),
+      outbound.close(),
+      inbound.close()
+    ])
+
+    const transferStats = (metrics as any).transferStats as Map<string, number>
+    expect(transferStats.get('global received')).to.equal(data.length)
   })
 
   it('should retain metrics after stop', async () => {
