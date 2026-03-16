f815b8d9914f4ceb48663e41949d686466e9c014
Faolain <Faolain@users.noreply.github.com>
2026-03-15 23:00:52 -0400

    tune SCTP buffers asymmetrically on Node: 8MiB send buffer with 4MiB receive buffer; improved webrtc-direct throughput ratio to ~0.0733\n\nResult: {"status":"keep","webrtc_direct_tcp_ratio":0.07327888257762595}

--- packages/transport-webrtc/src/webrtc/index.ts
+++ packages/transport-webrtc/src/webrtc/index.ts
@@ -2,7 +2,7 @@ import { setSctpSettings } from 'node-datachannel'
 
 setSctpSettings({
   sendBufferSize: 8 * 1024 * 1024,
-  recvBufferSize: 8 * 1024 * 1024
+  recvBufferSize: 4 * 1024 * 1024
 })
 
 export { RTCSessionDescription, RTCIceCandidate, RTCPeerConnection } from 'node-datachannel/polyfill'
