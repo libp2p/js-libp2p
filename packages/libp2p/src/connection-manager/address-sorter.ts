4b5292506d615eb2c095f9bf4471a913088d895d
tabcat <tabcat00@proton.me>
2026-05-07 13:14:38 +0700

    refactor(libp2p): drop asMultiaddr, use composite sort
    defaultAddressSorter now uses a single .sort() with a ||-chained
    composite comparator instead of five chained .sort() calls. Stable
    sort makes the two forms equivalent; the priority order
    (loopback > public > circuit > certified > reliable) is preserved.

--- packages/libp2p/src/connection-manager/address-sorter.ts
+++ packages/libp2p/src/connection-manager/address-sorter.ts
@@ -5,10 +5,6 @@ import {
   circuitRelayAddressesLast
 } from '@libp2p/utils'
 import type { Address } from '@libp2p/interface'
-import type { Multiaddr } from '@multiformats/multiaddr'
-
-const asMultiaddr = (fn: (a: Multiaddr, b: Multiaddr) => -1 | 0 | 1) =>
-  (a: Address, b: Address): -1 | 0 | 1 => fn(a.multiaddr, b.multiaddr)
 
 /**
  * Compare function for array.sort() that moves certified addresses to the start
@@ -25,10 +21,11 @@ export function certifiedAddressesFirst (a: Address, b: Address): -1 | 0 | 1 {
 }
 
 export function defaultAddressSorter (addresses: Address[]): Address[] {
-  return addresses
-    .sort(asMultiaddr(reliableTransportsFirst))
-    .sort(certifiedAddressesFirst)
-    .sort(asMultiaddr(circuitRelayAddressesLast))
-    .sort(asMultiaddr(publicAddressesFirst))
-    .sort(asMultiaddr(loopbackAddressLast))
+  return addresses.sort((a, b) =>
+    loopbackAddressLast(a.multiaddr, b.multiaddr) ||
+    publicAddressesFirst(a.multiaddr, b.multiaddr) ||
+    circuitRelayAddressesLast(a.multiaddr, b.multiaddr) ||
+    certifiedAddressesFirst(a, b) ||
+    reliableTransportsFirst(a.multiaddr, b.multiaddr)
+  )
 }
