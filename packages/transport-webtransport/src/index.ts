e27238a836431e575a12ef6164e31c30f3f4533b
achingbrain <alex@achingbrain.net>
2026-05-29 15:50:24 +0300

    fix: allow dialing WebTransport without PeerId
    PeerIds can be exchanged during the noise handshake so allow dialing
    a remote peer over WebTransport without a PeerId in the multiaddr.

--- packages/transport-webtransport/src/index.ts
+++ packages/transport-webtransport/src/index.ts
@@ -205,13 +205,19 @@ class WebTransportTransport implements Transport<WebTransportDialEvents> {
         log: this.components.logger.forComponent('libp2p:webtransport:connection')
       })
 
-      authenticated = await this.authenticateWebTransport({
+      const securePeer = await this.authenticateWebTransport({
         wt,
         maConn,
         remotePeer,
         certhashes,
         ...options
       })
+
+      authenticated = true
+
+      if (remotePeer != null) {
+        authenticated = remotePeer.equals(securePeer)
+      }
 
       if (!authenticated) {
         throw new InvalidCryptoExchangeError('Failed to authenticate webtransport')
@@ -220,7 +226,7 @@ class WebTransportTransport implements Transport<WebTransportDialEvents> {
       return await options.upgrader.upgradeOutbound(maConn, {
         ...options,
         skipEncryption: true,
-        remotePeer,
+        remotePeer: securePeer,
         muxerFactory: webtransportMuxer(wt),
         skipProtection: true
       })
@@ -243,7 +249,7 @@ class WebTransportTransport implements Transport<WebTransportDialEvents> {
     }
   }
 
-  async authenticateWebTransport ({ wt, maConn, remotePeer, certhashes, onProgress, signal }: AuthenticateWebTransportOptions): Promise<boolean> {
+  async authenticateWebTransport ({ wt, maConn, remotePeer, certhashes, onProgress, signal }: AuthenticateWebTransportOptions): Promise<PeerId> {
     onProgress?.(new CustomProgressEvent('webtransport:open-authentication-stream'))
     const stream = await wt.createBidirectionalStream()
     signal?.throwIfAborted()
@@ -256,7 +262,7 @@ class WebTransportTransport implements Transport<WebTransportDialEvents> {
     const n = noise()(this.components)
 
     onProgress?.(new CustomProgressEvent('webtransport:secure-outbound-connection'))
-    const { remoteExtensions } = await n.secureOutbound(messages, {
+    const { remoteExtensions, remotePeer: securePeer } = await n.secureOutbound(messages, {
       signal,
       remotePeer,
       skipStreamMuxerNegotiation: true
@@ -275,7 +281,7 @@ class WebTransportTransport implements Transport<WebTransportDialEvents> {
       throw new InvalidParametersError("Our certhashes are not a subset of the remote's reported certhashes")
     }
 
-    return true
+    return securePeer
   }
 
   createListener (options: CreateListenerOptions): Listener {
