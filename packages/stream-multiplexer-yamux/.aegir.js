0905a5c9605aae18b5951ae59dbe560c1249dc17
tabcat <tabcat00@proton.me>
2026-09-02 00:48:06 -0500

    fix(yamux): address review findings
    Adds sideEffects false and module-sync export conditions, removes the obsolete low-level API doc example and dead benchmark harness, bumps bundlesizeMax to match the actual bundle.

--- packages/stream-multiplexer-yamux/.aegir.js
+++ packages/stream-multiplexer-yamux/.aegir.js
@@ -2,6 +2,6 @@
 /** @type {import('aegir/types').PartialOptions} */
 export default {
   build: {
-    bundlesizeMax: '9.5kB'
+    bundlesizeMax: '13kB'
   }
 }
--- packages/stream-multiplexer-yamux/src/index.ts
+++ packages/stream-multiplexer-yamux/src/index.ts
@@ -18,65 +18,6 @@
  *   ]
  * })
  * ```
- *
- * @example Using the low-level API
- *
- * ```js
- * import { yamux } from '@libp2p/yamux'
- * import { pipe } from 'it-pipe'
- * import { duplexPair } from 'it-pair/duplex'
- * import all from 'it-all'
- *
- * // Connect two yamux muxers to demo basic stream multiplexing functionality
- *
- * const clientMuxer = yamux({
- *   client: true,
- *   onIncomingStream: stream => {
- *     // echo data on incoming streams
- *     pipe(stream, stream)
- *   },
- *   onStreamEnd: stream => {
- *     // do nothing
- *   }
- * })()
- *
- * const serverMuxer = yamux({
- *   client: false,
- *   onIncomingStream: stream => {
- *     // echo data on incoming streams
- *     pipe(stream, stream)
- *   },
- *   onStreamEnd: stream => {
- *     // do nothing
- *   }
- * })()
- *
- * // `p` is our "connections", what we use to connect the two sides
- * // In a real application, a connection is usually to a remote computer
- * const p = duplexPair()
- *
- * // connect the muxers together
- * pipe(p[0], clientMuxer, p[0])
- * pipe(p[1], serverMuxer, p[1])
- *
- * // now either side can open streams
- * const stream0 = clientMuxer.newStream()
- * const stream1 = serverMuxer.newStream()
- *
- * // Send some data to the other side
- * const encoder = new TextEncoder()
- * const data = [encoder.encode('hello'), encoder.encode('world')]
- * pipe(data, stream0)
- *
- * // Receive data back
- * const result = await pipe(stream0, all)
- *
- * // close a stream
- * stream1.close()
- *
- * // close the muxer
- * clientMuxer.close()
- * ```
  */
 
 import { Yamux } from './muxer.ts'
